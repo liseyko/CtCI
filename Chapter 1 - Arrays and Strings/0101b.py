7b8fa7266727e658ca06c1d58aa7b8bd784f87cb
Alexander Liseyko <liseyko@gmail.com>
2018-06-27 17:22:16 -0700

    cleanup

--- Chapter 1 - Arrays and Strings/0101b.py
+++
@@ -1,9 +0,0 @@
-print("Enter a test string:")
-istr = sorted(input())
-#print(istr)
-
-for i in range(1,len(istr)):
-  if istr[i] == istr[i-1]:
-    print("This string has a duplicate character: ", istr[i])
-    exit()
-print("All characters are unique.")
