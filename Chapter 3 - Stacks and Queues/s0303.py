d1931cfdf74cd91b46a44154105dbf4fde807fab
Alexander Liseyko <liseyko@gmail.com>
2018-07-07 03:11:26 -0700

    3.3 space optimization

--- Chapter 3 - Stacks and Queues/s0303.py
+++ Chapter 3 - Stacks and Queues/s0303.py
@@ -1,42 +1,105 @@
 from stack import Stack
+from linkedlist import Node
+
+
+class DNode(Node):
+    def __init__(self,data=None,next=None,prev=None):
+        self.prev = prev
+        super().__init__(data,next)
+
+class ExtendedStack(Stack):
+    """ stack implementation"""
+    def __init__(self,data = []):
+        super().__init__(data)
+
+    def push(self,data):
+        self.head = DNode(data,self.head)
+        if self.head.next is None:
+            self.tail = self.head
+        else:
+            self.head.next.prev = self.head
+        self.len += 1
+
+    def pop(self):
+        data = super().pop()
+        if self.head is None:
+            self.tail = None
+        else:
+            self.head.prev = None
+        return data
+
+    def popBottom(self):
+        data = None
+        if self.tail:
+            data = self.tail.data
+            if self.tail.prev:
+                self.tail = self.tail.prev
+                #self.tail.next.prev = None
+                self.tail.next = None
+            else:
+                self.head = None
+                self.tail = None
+            self.len -= 1
+        return data
+
 
 class SetOfStacks():
     def __init__(self,data=[],sizeOfStack=8):
         self.sizeOfStack = sizeOfStack
-        #self.numberOfStacks = 1
-        self.stacks = [Stack()]
-        self.cur_stack = 0
-        self.cur_stack_len = 0
+        self.stacks = [ExtendedStack()]
+        self.wasted_space = 0
         for d in data:
             self.push(d)
 
     def push(self,data):
-        if self.cur_stack_len < self.sizeOfStack:
-            self.stacks[self.cur_stack].push(data)
-            self.cur_stack_len += 1
+        if len(self.stacks[-1]) < self.sizeOfStack:
+            self.stacks[-1].push(data)
         else:
-            self.stacks.append(Stack())
-            self.cur_stack += 1
-            self.cur_stack_len = 0
+            self.stacks.append(ExtendedStack())
             self.push(data)
 
     def pop(self):
-        data = self.stacks[self.cur_stack].pop()
+        data = self.stacks[-1].pop()
         if data is None:
             return data
-        self.cur_stack_len -= 1
-        if self.cur_stack_len == 0 and self.cur_stack > 0:
-            del(self.stacks[self.cur_stack])
-            self.cur_stack -= 1
-            self.cur_stack_len = len(self.stacks[self.cur_stack])
+        if len(self.stacks[-1]) == 0 and len(self.stacks) > 1:
+            del(self.stacks[-1])
         return data
 
     def popAt(self,index):
-        if index > self.cur_stack:
+        """optimal time, with optimisation of wasted space"""
+        if index > len(self.stacks)-1:
             return None
+        elif index == len(self.stacks)-1:
+            return self.pop()
         data = self.stacks[index].pop()
-        if len(self.stacks[index]) == 0 and self.cur_stack > 0:
+        if len(self.stacks[index]) == 0 and len(self.stacks) > 1:
+            self.wasted_space -= self.sizeOfStack
             del(self.stacks[index])
-            self.cur_stack -= 1
+        else:
+            self.wasted_space += 1
+            if self.wasted_space > self.sizeOfStack * len(self.stacks) // 4 + 1:
+                self.shrink() # optimize after wasting more than 25% of space
         return data
+
+    def shiftleft(self,sl,sr):
+        while len(sl) < self.sizeOfStack:
+            data = sr.popBottom()
+            if data is None:
+                self.stacks.remove(sr)
+                return False
+            sl.push(data)
+        if len(sr) == 0:
+            self.stacks.remove(sr)
+        return True
+
+    def shrink(self):
+        i = 0
+        while i < len(self.stacks) - 1:
+            if len(self.stacks[i]) == self.sizeOfStack:
+                i += 1
+                continue
+            else:
+                if (self.shiftleft(self.stacks[i],self.stacks[i+1])):
+                    i += 1
 
--- Chapter 3 - Stacks and Queues/test.py
+++ Chapter 3 - Stacks and Queues/test.py
@@ -11,28 +11,58 @@ for i in [3]:
 class S0103TestCase(unittest.TestCase):
     """ Tests for: SetOfStacks class with methods: push, pop, popAt"""
     def setUp(self):
-        self.ss = sol[303].SetOfStacks()
-        for i in range(14):
-            self.ss.push(i)
-        for _ in range(10):
-            self.ss.pop()
-        for i in range(15):
-            self.ss.push(i)
-
+        pass
+  
     def test_push_and_pop(self):
-        self.assertEqual(str(self.ss.stacks[0]),'[3, 2, 1, 0, 3, 2, 1, 0]')
+        ss = sol[303].SetOfStacks()
+        for i in range(14):
+            ss.push(i)
+        for _ in range(10):
+            ss.pop()
+        for i in range(15):
+            ss.push(i)
+        self.assertEqual(str(ss.stacks[0]),'[3, 2, 1, 0, 3, 2, 1, 0]')
         for _ in range(7):
-            self.ss.popAt(1)
-        self.assertEqual(str(self.ss.stacks[1]),'[4]')            
+            ss.popAt(1)
+        self.assertEqual(str(ss.stacks[1]),'[4]')            
         for _ in range(6):
-            self.ss.pop()
+            ss.pop()
         testdata = [1, 0, 3, 2, 1, 0]
-        self.assertEqual(str(self.ss.stacks[0]),str(testdata))            
+        self.assertEqual(str(ss.stacks[0]),str(testdata))            
         for i in range(6):
-            self.assertEqual(self.ss.pop(),testdata[i])
-        self.assertFalse(self.ss.pop())
-        self.assertEqual(str(self.ss.stacks[0]),'[]')
-        self.ss.push(1)
-        self.assertEqual(self.ss.pop(),1)
+            self.assertEqual(ss.pop(),testdata[i])
+        self.assertFalse(ss.pop())
+        self.assertEqual(str(ss.stacks[0]),'[]')
+        ss.push(1)
+        self.assertEqual(ss.pop(),1)
+
+    def test_shrinking(self):
+        ss = sol[303].SetOfStacks()
+        expected_results = [[9, 8, 5, 4, 3, 2, 1, 0], \
+                    [19, 18, 17, 16, 13, 12, 11, 10], \
+                    [29, 28, 27, 26, 25, 24, 21, 20], \
+                    [41, 40, 37, 36, 35, 34, 33, 32], \
+                    [51, 50, 49, 48, 45, 44, 43, 42], \
+                    [61, 60, 59, 58, 57, 56, 53, 52]]
+        for i in range(64):
+            ss.push(i)
+        for i in range(8):
+            ss.popAt(i)
+            ss.popAt(i)
+        ss.shrink()
+        for i in range(len(ss.stacks)):
+            self.assertEqual(str(ss.stacks[i]),str(expected_results[i]))
+
+    def test_auto_shrinking(self):
+        ss = sol[303].SetOfStacks()
+        for i in range(64):
+            ss.push(i)
+        for i in range(8):
+            ss.popAt(i)
+            ss.popAt(i)
+            ss.popAt(i)
+        for i in range(len(ss.stacks)):
+            self.assertEqual(str(ss.stacks[5]),"[63, 62, 61, 60, 59, 58]")
+
 
 unittest.main()
