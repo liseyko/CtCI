2021eaa6a07b3622797ef142d7340b80761a08a3
Alexander Liseyko <liseyko@gmail.com>
2018-08-04 01:49:47 -0700

    4.5 + alternative solution

--- Chapter 4 - Trees and Graphs/s0405.py
+++ Chapter 4 - Trees and Graphs/s0405.py
@@ -33,9 +33,29 @@ def isBT_BST(t):
         a, b = b, find_next(b)
     return True
 
+def _isBT_BST_2(btn, t_min = float('-inf'), t_max = float('inf')):
+    #print(t_min,'<',btn.key,'<',t_max)
+    if btn.key <= t_min or btn.key > t_max:
+        return False
+    l,r = True, True
+    if btn.l:
+        l = _isBT_BST_2(btn.l, t_min, btn.key)
+    if btn.r:
+        r = _isBT_BST_2(btn.r, btn.key, t_max)
+    return l and r
+
+
+def isBT_BST_2(t):
+    if not t.root:
+        return False
+    return _isBT_BST_2(t.root, float('-inf'), float('inf'))
+
+
 if __name__ == '__main__':
     t = AVLTree([1,2,3,4,5,6,7])
     print(isBT_BST(t))
+    print(isBT_BST_2(t))
 
     t.root.l.r.key=5
     print(isBT_BST(t))
+    print(isBT_BST_2(t))
