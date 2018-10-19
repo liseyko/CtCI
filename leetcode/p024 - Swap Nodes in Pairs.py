c469bfed4ca30fa3012b6daefd5f44d21526b65c
Alexander Liseyko <liseyko@gmail.com>
2018-10-18 23:42:50 -0700

    +1 	new file:   p024 - Swap Nodes in Pairs.py

---
+++ leetcode/p024 - Swap Nodes in Pairs.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # 1->2->3->4 => 2->1->4->3
+        # h->1->2<-t
+        if not head: return None
+        if not head.next: return head
+        h, t = head, head.next
+        head, p = head.next, ListNode(-1)
+        while h and t:
+            h.next = t.next
+            t.next = h
+            p.next = t
+            p = h
+            h = h.next
+            if h: t = h.next
+
+        return head
