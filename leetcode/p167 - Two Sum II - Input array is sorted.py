0f6785be2034453baef1c3e5aad4c714779e0cdf
Alexander Liseyko <liseyko@gmail.com>
2018-11-02 16:38:32 -0700

    +1 	new file:   p167 - Two Sum II - Input array is sorted.py

---
+++ leetcode/p167 - Two Sum II - Input array is sorted.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i, j = 0, len(numbers)-1
+
+        while i < j:
+            s = numbers[i] + numbers[j]
+            if s > target:
+                j -= 1
+            elif s < target:
+                i += 1
+            else:
+                return [i+1, j+1]
