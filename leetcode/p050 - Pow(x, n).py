f70a597dfeda130be8753004bfe91d86811116f7
Alexander Liseyko <liseyko@gmail.com>
2018-11-09 11:20:49 -0800

    +1 	modified:   p050 - Pow(x, n).py

--- leetcode/p050 - Pow(x, n).py
+++ leetcode/p050 - Pow(x, n).py
@@ -5,21 +5,31 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        #intmin, intmax = -2**31, 2**31-1
-        r, neg, sign = x, False, 1
-        if not n: return 1
-        if n < 0:
-            neg = True
-            n = abs(n)
-            
-        if x < 0 and n % 2 != 0:
-            sign = -1
-        
-        if abs(x) == 1: return abs(x) * sign
-        
-        while n > 1 and r and float('-inf') < r < float('inf'):
-            r *= x
-            n -= 1
+        if n < 0: return 1 / x * self.myPow(1 / x, -(n + 1))
+        if n == 0: return 1
+        if n == 2: return x * x
+        if n % 2 == 0: return self.myPow( self.myPow(x, n / 2), 2)
+        else: return x * self.myPow( x, n - 1)
 
-        if neg: r = 1 / r
+    def myPow(self, x, n):
+        if n == 0: return 1
+        if n < 0:
+            n = - n
+            x = 1 / x
+        if n % 2:
+            return self.myPow(x * x, n // 2) * x
+        else:
+            return self.myPow(x * x, n // 2)
+
+    def myPow(self, x, n):
+        if n == 0: return 1
+        if n < 0:
+            n = - n
+            x = 1 / x
+        r = 1
+        while n > 0:
+            if n & 1: r *= x
+            x *= x
+            n >>= 1
+
         return r
