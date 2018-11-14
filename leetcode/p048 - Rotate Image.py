1b11906da6ae8d1ed07ec77c58c4abdc1be0d518
Alexander Liseyko <liseyko@gmail.com>
2018-11-13 16:44:09 -0800

    modified:   p048 - Rotate Image.py

--- leetcode/p048 - Rotate Image.py
+++ leetcode/p048 - Rotate Image.py
@@ -11,3 +11,30 @@ class Solution:
             for i in range(j, n-j):
                 matrix[j][i], matrix[i][n-j], matrix[n-j][n-i], matrix[n-i][j] = \
                 matrix[n-i][j], matrix[j][i], matrix[i][n-j], matrix[n-j][n-i]
+
+    def rotate(self, A):
+        A[:] = map(list, zip(*A[::-1]))
+
+    def rotate(self, A):
+        A[:] = [[row[i] for row in A[::-1]] for i in range(len(A))]
+
+    def rotate(self, A):
+        A.reverse()
+        for i in range(len(A)):
+            for j in range(i):
+                A[i][j], A[j][i] = A[j][i], A[i][j]
+
+    def rotate(self, A):
+        n = len(A)
+        for i in range(n/2):
+            for j in range(n-n/2):
+                A[i][j], A[~j][i], A[~i][~j], A[j][~i] = \
+                         A[~j][i], A[~i][~j], A[j][~i], A[i][j]
+
+    def rotate(self, A):
+        n = len(A)
+        for i in range(n/2):
+            for j in range(n-n/2):
+                for _ in '123':
+                    A[i][j], A[~j][i], i, j = A[~j][i], A[i][j], ~j, ~i
+                i = ~j
