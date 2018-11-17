6e891d944efb1fea2000c8ef0a565735119349c9
Alexander Liseyko <liseyko@gmail.com>
2018-11-16 16:46:14 -0800

    +1 	new file:   p172 - Factorial Trailing Zeroes.py

---
+++ leetcode/p172 - Factorial Trailing Zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        r = 0
+        while n > 0:
+            n //= 5
+            r += n
+            
+        return r
