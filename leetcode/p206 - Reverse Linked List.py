479aa97dd52521f2efd479bd82b1f5b264107003
Alexander Liseyko <liseyko@gmail.com>
2018-11-26 22:01:31 -0800

    +1 	new file:   p206 - Reverse Linked List.py

---
+++ leetcode/p206 - Reverse Linked List.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next: return head
+        
+        p, c, n = None, head, head.next
+        while n:
+            c.next = p
+            p = c
+            c = n
+            n = n.next
+        c.next = p
+        return c
+
+    def reverseList(self, head):
+        if not head or not head.next: return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
