108356904c9876e7bdd265059791e2e37024cdf5
Alexander Liseyko <liseyko@gmail.com>
2018-11-27 23:48:00 -0800

    +1 	new file:   p193 - Valid Phone Numbers.sh 	modified:   p195 - Tenth Line.sh

--- leetcode/p195 - Tenth Line.sh
+++ leetcode/p195 - Tenth Line.sh
@@ -1,1 +1,11 @@
+# Solution 2
+awk 'FNR == 10 {print }'  file.txt
+# OR
 awk 'NR == 10' file.txt
+
+# Solution 3
+sed -n 10p file.txt
+
+# Most efficient: Solution 4
+tail -n+10 file.txt | head -1
+head -10 file.txt | tail -1
