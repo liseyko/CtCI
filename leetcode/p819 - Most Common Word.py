0c33eb98d9287296cb949c1c37c13460288c152f
Alexander Liseyko <liseyko@gmail.com>
2018-11-27 22:10:45 -0800

    modified:   p819 - Most Common Word.py

--- leetcode/p819 - Most Common Word.py
+++ leetcode/p819 - Most Common Word.py
@@ -6,12 +6,11 @@ class Solution:
         :type banned: List[str]
         :rtype: str
         """
-        wc = {}
-        for w in re.split('[\';?!,. ]\s*', paragraph.lower()):
-            if not w or w in banned: continue
+        wc, banned = {}, set(banned)
+       
+        for w in re.split("[;?!',. ]\s*", paragraph.lower().strip(";?!',. ")):
+            if w in banned: continue
             if w in wc: wc[w] += 1
             else: wc[w] = 1
-        #print(sorted(wc.items(), key = lambda x: x[1]))
-        
-        
+
         return sorted(wc.items(), key = lambda x: x[1])[-1][0]
