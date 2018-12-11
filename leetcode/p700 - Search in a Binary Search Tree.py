8325e95e6c0fa1a84b2d2900db93c149334336ff
Alexander Liseyko <liseyko@gmail.com>
2018-12-11 15:54:29 -0800

    +1 	new file:   p700 - Search in a Binary Search Tree.py

---
+++ leetcode/p700 - Search in a Binary Search Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        while root:
+            if root.val == val:
+                return root
+            elif val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return None
