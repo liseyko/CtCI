120285e25517685a2f1ab4ef455fb5e76cd19d96
Alexander Liseyko <liseyko@gmail.com>
2018-12-12 14:54:23 -0800

    modified:   p496 - Next Greater Element I.py

--- leetcode/p496 - Next Greater Element I.py
+++ leetcode/p496 - Next Greater Element I.py
@@ -1,19 +1,10 @@
 class Solution:
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        if not nums1 or not nums2: return []
+    def nextGreaterElement(self, findNums, nums):
+        st, d = [], {}
+    
+        for v in nums:
+            while st and st[-1] < v:
+                d[st.pop()] = v
+            st.append(v)
         
-        d = {nums2[-1]: -1}
-        for i in range(len(nums2)-2,-1,-1):
-            n = nums2[i]
-            nxt = i+1
-            while nxt != -1 and nums2[nxt] <= n:
-                nxt = d[nums2[nxt]]
-            d[n] = nxt
-        #print(d)
-        nums2.append(-1)
-        return [nums2[d[i]] for i in nums1]
+        return [d.get(x, -1) for x in findNums]
