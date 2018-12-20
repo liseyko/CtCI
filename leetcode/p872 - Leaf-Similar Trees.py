ec12a276c5c15c1ab648c56bbb3f50d31d180071
Alexander Liseyko <liseyko@gmail.com>
2018-12-19 16:31:32 -0800

    +1 	new file:   p872 - Leaf-Similar Trees.py

---
+++ leetcode/p872 - Leaf-Similar Trees.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        def getleaf(q):
+            while q:
+                node = q.pop()
+                if not node.left and not node.right:
+                    yield node.val
+                else:
+                    q.extend([c for c in (node.right, node.left) if c])
+
+        for l1, l2 in zip(getleaf([root1]), getleaf([root2])):
+            if l1 != l2:
+                return False
+        return True
