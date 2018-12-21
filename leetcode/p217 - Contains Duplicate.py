f55a21b187ce3ac02d0cac36db7b44a57d479811
Alexander Liseyko <liseyko@gmail.com>
2018-12-20 22:23:16 -0800

    +1 	new file:   p217 - Contains Duplicate.py

---
+++ leetcode/p217 - Contains Duplicate.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set()
+        for n in nums:
+            if n in s:
+                return True
+            s.add(n)
+
+        return False
+
+    def containsDuplicate(self, nums):
+        return len(set(nums)) < len(nums)
