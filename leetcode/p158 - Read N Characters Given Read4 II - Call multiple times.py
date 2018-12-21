5bae18babe0138d8d732d1c5aed8aa0a4179d9a6
Alexander Liseyko <liseyko@gmail.com>
2018-12-20 16:32:29 -0800

    modified:   p158 - Read N Characters Given Read4 II - Call multiple times.py

--- leetcode/p158 - Read N Characters Given Read4 II - Call multiple times.py
+++ leetcode/p158 - Read N Characters Given Read4 II - Call multiple times.py
@@ -3,11 +3,13 @@
 # @return an integer
 # def read4(buf):
 
+
 class Solution(object):
+
     def __init__(self):
-        self.buf = [None] * 4
-        self.buflen = 0
-        
+        self.buf4 = [None] * 4
+        self.buf4len = 0
+
     def read(self, buf, n):
         """
         :type buf: Destination buffer (List[str])
@@ -16,33 +18,14 @@ class Solution(object):
         """
         i = 0
         while i < n:
-            if not self.buflen:
-                self.buflen = read4(self.buf)
-                if not self.buflen:
+            if not self.buf4len:
+                self.buf4len = read4(self.buf4)
+                if not self.buf4len:
                     break
-            actual_size = min(n - i, self.buflen)
-            buf[i:] = self.buf[:actual_size]
-            i += actual_size
-            
-            if actual_size < self.buflen:
-                self.buf = self.buf[actual_size:self.buflen]
-                self.buflen = len(self.buf)
-                for _ in range(self.buflen, 4):
-                    self.buf.append(None)
-            else:
-                self.buflen = 0
-            
+            cur_size = min(n - i, self.buf4len)
+            buf[i:] = self.buf4[:cur_size]
+            i += cur_size
+            self.buf4[:self.buf4len-cur_size] = self.buf4[cur_size:self.buf4len]
+            self.buf4len = self.buf4len - cur_size
+
         return i
-    
-    def __init__(self):
-        self.queue = []
-        
-    def read(self, buf, n):
-        read, need, buffer = 0, n, [None]*4
-        while need > 0:
-            k = read4(buffer)
-            self.queue.extend(buffer[:k])
-            need = min(len(self.queue), n - read)
-            buf[read:read+need] = [self.queue.pop(0) for _ in xrange(need)]
-            read += need
-        return read
