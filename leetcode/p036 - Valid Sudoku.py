46076c3beebf1260568de2a6356f698613c520da
Alexander Liseyko <liseyko@gmail.com>
2018-12-25 17:01:14 -0800

    modified:   p036 - Valid Sudoku.py

--- leetcode/p036 - Valid Sudoku.py
+++ leetcode/p036 - Valid Sudoku.py
@@ -17,3 +17,21 @@ class Solution:
                     else: 
                         s.add(board[row][col])
         return True
+
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """                
+        rows, cols = [set() for _ in range(9)], [set() for _ in range(9)]
+        squares = [[set() for _ in range(3)] for _ in range(3)]
+        for j, row in enumerate(board):
+            for i, col in enumerate(row):
+                if board[j][i] != '.':
+                    if board[j][i] in rows[j] | cols[i] | squares[j // 3][i // 3]:
+                        return False
+                    rows[j].add(board[j][i])
+                    cols[i].add(board[j][i])
+                    squares[j//3][i//3].add(board[j][i])
+
+        return True
