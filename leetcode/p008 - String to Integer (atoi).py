b818f117893d18a5355bfa1795c3831701ff9e27
Alexander Liseyko <liseyko@gmail.com>
2018-12-25 18:08:12 -0800

    modified:   p008 - String to Integer (atoi).py

--- leetcode/p008 - String to Integer (atoi).py
+++ leetcode/p008 - String to Integer (atoi).py
@@ -32,3 +32,29 @@ class Solution(object):
             i += 1
 
         return sign*n
+
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        INT_MIN, INT_MAX = 2**31, 2**31 - 1
+        limits = [None, INT_MAX, INT_MIN]
+        signs = {'-': -1, '+': 1}
+
+        i = res = 0
+        sign, limit = 1, divmod(INT_MAX, 10)
+        while i < len(str)-1 and str[i] == ' ':
+            i += 1
+        if i < len(str)-1 and str[i] in signs:
+            sign, limit = signs[str[i]], divmod(limits[signs[str[i]]], 10)
+            i += 1
+
+        while i < len(str) and ord('0') <= ord(str[i]) <= ord('9'):
+            digit = ord(str[i])-ord('0')
+            if res >= limit[0] and res - limit[0] + digit >= limit[1]:
+                return sign * limits[sign]
+            res = res * 10 + digit
+            i += 1
+
+        return sign * res
