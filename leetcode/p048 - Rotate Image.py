52472377f25eb6c0e94993ba306dc542e2f07dd0
Alexander Liseyko <liseyko@gmail.com>
2018-12-25 16:32:08 -0800

    modified:   p048 - Rotate Image.py

--- leetcode/p048 - Rotate Image.py
+++ leetcode/p048 - Rotate Image.py
@@ -1,16 +1,13 @@
 class Solution:
-    def rotate(self, matrix):
+    def rotate(self, A):
         """
-        :type matrix: List[List[int]]
+        :type A: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix) - 1
-        n2 = len(matrix) // 2 + len(matrix) % 2
-        
-        for j in range(n2):
-            for i in range(j, n-j):
-                matrix[j][i], matrix[i][n-j], matrix[n-j][n-i], matrix[n-i][j] = \
-                matrix[n-i][j], matrix[j][i], matrix[i][n-j], matrix[n-j][n-i]
+        for j in range(len(A)//2):
+            for i in range(j,len(A)-1-j):
+                A[i][~j], A[~j][~i], A[~i][j], A[j][i] = \
+                A[j][i], A[i][~j], A[~j][~i], A[~i][j]
 
     def rotate(self, A):
         A[:] = map(list, zip(*A[::-1]))
@@ -30,11 +27,3 @@ class Solution:
             for j in range(n-n/2):
                 A[i][j], A[~j][i], A[~i][~j], A[j][~i] = \
                          A[~j][i], A[~i][~j], A[j][~i], A[i][j]
-
-    def rotate(self, A):
-        n = len(A)
-        for i in range(n/2):
-            for j in range(n-n/2):
-                for _ in '123':
-                    A[i][j], A[~j][i], i, j = A[~j][i], A[i][j], ~j, ~i
-                i = ~j
