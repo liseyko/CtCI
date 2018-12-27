6fc41519f69c7f09e0b3892ad59ff2fd5e5a56a1
Alexander Liseyko <liseyko@gmail.com>
2018-12-26 17:33:40 -0800

    modified:   p190 - Reverse Bits.py

--- leetcode/p190 - Reverse Bits.py
+++ leetcode/p190 - Reverse Bits.py
@@ -1,9 +1,33 @@
 class Solution:
     # @param n, an integer
     # @return an integer
+    def __init__(self):
+        self.cache = {0:0}
+
     def reverseBits(self, n):
+        
         r = 0
         for _ in range(32):
             r = (r << 1) + (n & 1)
             n >>= 1
+        return r
+
+    def reverseByte(self, byte):
+        if byte not in self.cache:
+            n, r = byte, 0
+            for _ in range(8):
+                r = (r << 1) + (n & 1)
+                n >>= 1
+            self.cache[byte] = r
+        return self.cache[byte]
+
+    def reverseBits(self, n):
+        bytes = [0] * 4
+        for i in range(4):
+            bytes[i] = n & 255
+            n >>= 8
+        r = 0
+        print(bytes)
+        for b in bytes:
+            r = (r << 8) + self.reverseByte(b)
         return r
