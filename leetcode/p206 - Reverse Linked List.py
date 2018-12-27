192357f5ed7e09d02070fbd8163b1d87aca0d27c
Alexander Liseyko <liseyko@gmail.com>
2018-12-26 19:09:57 -0800

    modified:   p206 - Reverse Linked List.py

--- leetcode/p206 - Reverse Linked List.py
+++ leetcode/p206 - Reverse Linked List.py
@@ -4,37 +4,35 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        if not head or not head.next: return head
-        
-        p, c, n = None, head, head.next
-        while n:
-            c.next = p
-            p = c
-            c = n
-            n = n.next
-        c.next = p
-        return c
+        if not head or not head.next:
+            return head
+        prv = head
+        cur = head.next
+        prv.next = None
+        while cur:
+            nxt = cur.next
+            cur.next = prv
+            prv = cur
+            cur = nxt
+        return prv
 
     def reverseList(self, head):
-        if not head or not head.next: return head
+        if not head or not head.next:
+            return head
         p = self.reverseList(head.next)
         head.next.next = head
         head.next = None
         return p
-    
+
     def reverseList(self, head):
         prev, curr = None, head
-    
+
         while curr:
             next = curr.next
             curr.next = prev
-            prev = curr
-            curr = next
+            prev, curr = curr, next
 
         return prev
