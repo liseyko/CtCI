69529a26d67bf72a4a71e77da759ad230efba181
Alexander Liseyko <liseyko@gmail.com>
2018-12-26 18:40:16 -0800

    modified:   p242 - Valid Anagram.py

--- leetcode/p242 - Valid Anagram.py
+++ leetcode/p242 - Valid Anagram.py
@@ -10,10 +10,10 @@ class Solution:
 
     def isAnagram(self, s, t):
         return all([s.count(c)==t.count(c) for c in string.ascii_lowercase])
-    
+
     def isAnagram(self, s, t):
         if len(s) != len(t):
-            return False 
+            return False
         count = collections.defaultdict(int)
         for c in s:
             count[c] += 1
@@ -21,16 +21,4 @@ class Solution:
             count[c] -= 1
             if count[c] < 0:
                 return False
-        return True    
-    
-    def isAnagram(self, s, t):    
-        cnt = {}
-        for c in s:
-            cnt[c] = cnt.get(c,0) + 1
-                
-        for c in t:
-            if c not in cnt: return False
-            if cnt[c] == 1: del cnt[c]
-            else: cnt[c] -= 1
-        
-        return not cnt
+        return True
