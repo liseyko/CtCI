7e5e1861db16e5abc66c07c7de8076c6dc7b952b
Alexander Liseyko <liseyko@gmail.com>
2018-12-27 12:11:16 -0800

    modified:   p021 - Merge Two Sorted Lists.py

--- leetcode/p021 - Merge Two Sorted Lists.py
+++ leetcode/p021 - Merge Two Sorted Lists.py
@@ -1,25 +1,11 @@
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        if not l1:
-            return l2
-        if not l2:
-            return l1
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-        if l2.val < l1.val:
-            l1, l2 = l2, l1
-        head = l1
 
-        while l2:
-            while l1.next and l1.next.val < l2.val:
-                l1 = l1.next
-            l1.next, l2 = l2, l1.next
-            l1 = l1.next
-        return head
+class Solution:
 
     def mergeTwoLists(self, l1, l2):
         res = cur = ListNode(-1)
@@ -35,13 +21,27 @@ class Solution(object):
         return res.next
 
     def mergeTwoLists(self, l1, l2):
-        if not l1:
-            return l2
-        if not l2:
-            return l1
+        if not (l1 and l2):
+            return l1 or l2
         if l1.val < l2.val:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
         else:
             l2.next = self.mergeTwoLists(l2.next, l1)
             return l2
+
+    def mergeTwoLists(self, l1, l2):
+        if not (l1 and l2):
+            return l1 or l2
+
+        if l2.val < l1.val:
+            l1, l2 = l2, l1
+        head = l1
+
+        while l2:
+            while l1.next and l1.next.val < l2.val:
+                l1 = l1.next
+            l1.next, l2 = l2, l1.next
+            l1 = l1.next
+
+        return head
