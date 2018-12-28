364f1f454fc98af08287644c7edecebe13ed752f
Alexander Liseyko <liseyko@gmail.com>
2018-12-28 11:52:18 -0800

    modified:   p070 - Climbing Stairs.py

--- leetcode/p070 - Climbing Stairs.py
+++ leetcode/p070 - Climbing Stairs.py
@@ -1,24 +1,14 @@
 import math
 
+
 class Solution:
-    def __init__(self):
-        self.cache = [0,1,2]
-        self.sqrt5 = math.sqrt(5)
-        
     def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n < len(self.cache):
-            return self.cache[n]
-        
-        for _ in range(n - len(self.cache) + 1):
-            self.cache.append(  self.cache[-1] + self.cache[-2] )
-        
-        return self.cache[n]
-
+        cache = [1, 1]
+        for _ in range(2, n+1):
+            cache.append(cache[-1] + cache[-2])
+        return cache[n]
 
     def climbStairs(self, n):
-        fibn = pow((1 + self.sqrt5) / 2, n+1) - pow((1 - self.sqrt5) / 2, n+1);
-        return int(fibn // self.sqrt5)
+        sqrt5 = math.sqrt(5)
+        fibn = pow((1+sqrt5) / 2, n+1) - pow((1-sqrt5) / 2, n+1)
+        return int(fibn // sqrt5)
