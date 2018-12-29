8d370ea5a45fd648892d8c024f9c0a1320dec4f6
Alexander Liseyko <liseyko@gmail.com>
2018-12-29 14:38:02 -0800

    modified:   p155 - Min Stack.py

--- leetcode/p155 - Min Stack.py
+++ leetcode/p155 - Min Stack.py
@@ -1,20 +1,48 @@
 class MinStack:
-    
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.data = []
+        self.min = float('inf')
 
     def push(self, x):
         """
         :type x: int
         :rtype: void
         """
-        topMin = x
-        if self.data:
-            topMin = min(topMin, self.data[-1][1])
-        self.data.append((x, topMin))
+        if x <= self.min:
+            self.data.append(self.min)
+            self.min = x
+        self.data.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if self.min == self.data.pop():
+            self.min = self.data.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.data[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min
+
+
+class MinStack:
+    def __init__(self):
+        self.data = [(None, float('inf'))]
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.data.append((x, min(self.data[-1][1], x)))
 
     def pop(self):
         """
@@ -33,11 +61,3 @@ class MinStack:
         :rtype: int
         """
         return self.data[-1][1]
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
