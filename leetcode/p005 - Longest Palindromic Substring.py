1930bda194eb5ca99623004cd5375067571588ce
Alexander Liseyko <liseyko@gmail.com>
2019-01-02 15:56:22 -0800

    modified:   p005 - Longest Palindromic Substring.py

--- leetcode/p005 - Longest Palindromic Substring.py
+++ leetcode/p005 - Longest Palindromic Substring.py
@@ -1,29 +1,20 @@
 class Solution:
-    def __init__(self):
-        self.max_pal = None
-        self.i = 0
 
-    def chk_pal(self, s, ij):
-        l, r = ij
-        #print('chk:',l,r)
-        ln = len(s)
-        while l > 0 and r < ln-1 and s[l-1] == s[r+1]:
+    def chk_pal(self, s, lr):
+        l, r = lr
+        while l > 0 and r < len(s)-1 and s[l-1] == s[r+1]:
                 l -= 1
                 r += 1
-        #print('chkret:',l,r)
         return l, r
 
-    def upd_max(self,lr):
+    def upd_max(self, lr):
         l, r = lr
         if r - l + 1 > self.max_pal[0]:
-            self.max_pal = r-l+1,(l,r)
+            self.max_pal = r-l+1, (l, r)
 
-    def findSame(self,s,i):
-        #l, r, ln = i, i, len(s)
-        j, ln = i, len(s)
-        #while l > 0 and s[l-1] == s[i]:
-        #    l -= 1
-        while j < ln-1 and s[j+1] == s[i]:
+    def findSame(self, s, i):
+        j = i
+        while j < len(s)-1 and s[j+1] == s[i]:
             j += 1
             self.i = j
         return i, j
@@ -33,18 +24,42 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        ln = len(s)
-        if ln < 2:
-            return s
 
-        self.max_pal = 1,(0,0)
-
-        self.i = 0
-        while self.i < ln-1:
-            self.upd_max(self.chk_pal(s,self.findSame(s,self.i)))
+        self.i, self.max_pal = 0, (1, (0, 0))
+        while self.i < len(s)-1:
+            self.upd_max(self.chk_pal(s, self.findSame(s, self.i)))
             self.i += 1
 
         return s[self.max_pal[1][0]:self.max_pal[1][1]+1]
+
+
+class Solution:
+    """
+    Manacher algorithm
+    http://en.wikipedia.org/wiki/Longest_palindromic_substring
+    """
+    def longestPalindrome(self, s):
+        # Transform S into T.
+        # For example, S = "abba", T = "^a#b#b#a$".
+        # ^ and $ signs are added to avoid bounds checking
+        T = '#'.join('^{}$'.format(s))
+        n = len(T)
+        P = [0] * n
+        C = R = 0
+        for i in range(1, n-1):
+            P[i] = (R > i) and min(R - i, P[2*C - i])  # = i' = C - (i-C)
+            # Attempt to expand palindrome centered at i
+            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+                P[i] += 1
+
+            # If palindrome centered at i expand past R,
+            # adjust center based on expanded palindrome.
+            if i + P[i] > R:
+                C, R = i, i + P[i]
+
+        # Find the maximum element in P.
+        maxLen, centerIndex = max((n, i) for i, n in enumerate(P))
+        return s[(centerIndex - maxLen)//2: (centerIndex + maxLen)//2]
 
 
 if __name__ == '__main__':
@@ -56,5 +71,18 @@ if __name__ == '__main__':
     print(sol.longestPalindrome('cbbd'))
     print(sol.longestPalindrome('babad'))
     print(sol.longestPalindrome('abracadabrarbd'))
-    print(sol.longestPalindrome('"321012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210123210012321001232100123210123"'))
-
+    print(sol.longestPalindrome('"3210123210012321001232100123210012321001232\
+    1001232100123210012321001232100123210012321001232100123210012321001232100\
+    1232100123210012321001232100123210012321001232100123210012321012321001232\
+    1001232100123210012321001232100123210012321001232100123210012321001232100\
+    1232100123210012321001232100123210012321001232100123210012321001232100123\
+    2100123210012321001232100123210012321001232100123210012321001232100123210\
+    0123210012321001232100123210012321001232100123210012321001232100123210012\
+    3210012321001232100123210012321001232100123210012321001232100123210012321\
+    0012321001232100123210012321001232100123210012321001232100123210012321001\
+    2321001232100123210012321001232100123210012321001232100123210012321001232\
+    1001232100123210012321001232100123210012321001232100123210012321001232100\
+    1232100123210012321001232100123210012321001232100123210012321001232100123\
+    2100123210012321001232100123210012321001232100123210012321001232100123210\
+    0123210012321001232100123210012321001232100123210123210012321001232100123\
+    210123"'))
