c7aca63c28fe2120744d1efa954980fe496f8c44
Alexander Liseyko <liseyko@gmail.com>
2019-01-03 14:50:24 -0800

    +1 	modified:   p042 - Trapping Rain Water.py

--- leetcode/p042 - Trapping Rain Water.py
+++ leetcode/p042 - Trapping Rain Water.py
@@ -1,41 +1,15 @@
 class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        if len(height) < 3: return 0
-
-        r = 0
-        mxr = [h for h in height]
-        
-        for j in range(len(height) - 2, -1, -1):
-            mxr[j] = max(mxr[j],mxr[j+1])
-
-        mxl = height[0]
-        for i in range(1,len(height)-1):
-            mxl = max(mxl, height[i])
-            r += min(mxl, mxr[i]) - height[i]
-
-        return r
 
     def trap(self, height):
-        result = 0
-        l, r = 0, len(height) - 1
-        lmax = rmax = 0
-
-        while l < r:
-            if height[l] < height[r]:
-                if height[l] >= lmax:
-                    lmax = height[l]
-                else:
-                    result += lmax - height[l]
-                l += 1
+        lmax = rmax = res = 0
+        li, ri = 0, len(height)-1
+        while li <= ri:
+            if lmax < rmax:
+                lmax = max(lmax, height[li])
+                res += lmax - height[li]
+                li += 1
             else:
-                if height[r] >= rmax:
-                    rmax = height[r]
-                else:
-                    result += rmax - height[r]
-                r -= 1
-
-        return result
+                rmax = max(rmax, height[ri])
+                res += rmax - height[ri]
+                ri -= 1
+        return res
