30bec193487f076e99059bc6478f65a53db0f840
Alexander Liseyko <liseyko@gmail.com>
2019-01-03 22:32:23 -0800

    modified:   p023 - Merge k Sorted Lists.py

--- leetcode/p023 - Merge k Sorted Lists.py
+++ leetcode/p023 - Merge k Sorted Lists.py
@@ -1,5 +1,4 @@
 import heapq
-#from Queue import PriorityQueue
 # Definition for singly-linked list.
 """
 class ListNode:
@@ -8,34 +7,34 @@ class ListNode:
         self.next = None
 """
 
+
 class Solution(object):
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
+        Time/space complexity : O(Nlogk) / O(n+k)
         """
-        q, h = len(lists), []
-        for i in range(q):
-            if lists[i]:
-                heapq.heappush(h, (lists[i].val, i, lists[i]))
-        
-        rhead = rtail = ListNode(0)
-        
+        res = cur = ListNode(-1)
+        h = []
+        for i, l in enumerate(lists):
+            if l:
+                heapq.heappush(h, (l.val, i, l))
         while h:
             i, n = heapq.heappop(h)[1:]
-            rtail.next = n
-            rtail = rtail.next
+            cur.next = n
+            cur = cur.next
             if n.next:
                 heapq.heappush(h, (n.next.val, i, n.next))
-                
-        return rhead.next
-        #Time complexity : O(Nlogk)
-        #Space complexity : O(n+k)
 
-# TODO: Merge with Divide And Conquer
+        return res.next
+
+
 """
+TODO: Merge with Divide And Conquer
 Pair up k lists and merge each pair.
-After the first pairing, k lists are merged into k/2 lists with average 2N/k length, then k/4, k/8 and so on.
+After the first pairing, k lists are merged into k/2 lists with
+average 2N/k length, then k/4, k/8 and so on.
 Repeat this procedure until we get the final sorted linked list.
 Time complexity : O(Nlogk)
 Space complexity : O(1)
