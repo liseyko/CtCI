2e49c74e11f35e13d8061f7bd19f9a54c7723ff9
Alexander Liseyko <liseyko@gmail.com>
2019-01-06 02:14:01 -0800

    modified:   p970 - Powerful Integers.py

--- leetcode/p970 - Powerful Integers.py
+++ leetcode/p970 - Powerful Integers.py
@@ -6,16 +6,15 @@ class Solution:
         :type bound: int
         :rtype: List[int]
         """
-        vals = {}
-        for v in (x, y):
-            limit = int(math.log(bound-(x+y-v)**0, v)) + 1 if v > 1 else 2 - v
-            vals[v] = [v**i for i in range(limit)]
-        res = set()
-        for xv in vals[x]:
-            for yv in vals[y]:
-                r = xv + yv
-                if r > bound:
-                    break
-                res.add(r)
-
+        vals, res = {}, set()
+        if bound:
+            for v in (x, y):
+                lim = int(math.log(bound-(x+y-v)**0, v))+1 if v > 1 else 2 - v
+                vals[v] = [v**i for i in range(lim)]
+            for xv in vals[x]:
+                for yv in vals[y]:
+                    r = xv + yv
+                    if r > bound:
+                        break
+                    res.add(r)
         return list(res)
