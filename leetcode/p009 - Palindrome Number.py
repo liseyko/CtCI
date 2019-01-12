a85ed4bab45268ceba6b834d38857ddf005b5fca
Alexander Liseyko <liseyko@gmail.com>
2019-01-11 19:16:52 -0800

    modified:   p009 - Palindrome Number.py

--- leetcode/p009 - Palindrome Number.py
+++ leetcode/p009 - Palindrome Number.py
@@ -1,16 +1,9 @@
 class Solution:
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x == 0:
-            return True
-        elif x < 0 or x % 10 == 0:
+        if x < 0 or x > 0 and x % 10 == 0:
             return False
-        y = 0
-        while x > y:
-            y, x = y * 10 + x % 10, x // 10
-            print(x,y)
-
-        return x == y or x == y // 10
+        xrev = 0
+        while x > xrev:
+            xrev = xrev * 10 + x % 10
+            x //= 10
+        return x == xrev or x == xrev // 10
