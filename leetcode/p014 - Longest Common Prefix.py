90978836cc078af3eee0905263695f88e205252c
Alexander Liseyko <liseyko@gmail.com>
2019-01-17 16:42:28 -0800

    modified:   p014 - Longest Common Prefix.py

--- leetcode/p014 - Longest Common Prefix.py
+++ leetcode/p014 - Longest Common Prefix.py
@@ -1,24 +1,14 @@
 class Solution(object):
 
     def longestCommonPrefix(self, strs):
-        if not strs or not strs[0]:
-            return ""
-        i = -1
-        while True:
-            i += 1
-            if i < len(strs[0]):
-                c =  strs[0][i]
-            for s in strs:
-                if i == len(s) or c != s[i]:
-                    return s[0:i]
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        cw = strs[0] if strs else ""
 
-    def longestCommonPrefix(self, strs):
-        if not strs:
-            return ""
-
-        for i, c in enumerate(strs[0]):
+        for i, c in enumerate(cw):
             for w in strs[1:]:
                 if i == len(w) or w[i] != c:
-                    return w[:i]
-
-        return strs[0]
+                    return cw[:i]
+        return cw
