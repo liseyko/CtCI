4f0dcce8e717e26cf9f32693a0e6b3ae182cd8dd
Alexander Liseyko <liseyko@gmail.com>
2019-01-24 14:06:05 -0800

    modified:   p050 - Pow(x, n).py

--- leetcode/p050 - Pow(x, n).py
+++ leetcode/p050 - Pow(x, n).py
@@ -5,14 +5,8 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        if n < 0: return 1 / x * self.myPow(1 / x, -(n + 1))
-        if n == 0: return 1
-        if n == 2: return x * x
-        if n % 2 == 0: return self.myPow( self.myPow(x, n / 2), 2)
-        else: return x * self.myPow( x, n - 1)
-
-    def myPow(self, x, n):
-        if n == 0: return 1
+        if n == 0:
+            return 1
         if n < 0:
             n = - n
             x = 1 / x
@@ -22,22 +16,27 @@ class Solution(object):
             return self.myPow(x * x, n // 2)
 
     def myPow(self, x, n):
-        if n == 0: return 1
+        if n == 0:
+            return 1
         if n < 0:
             n = - n
             x = 1 / x
         r = 1
         while n > 0:
-            if n & 1: r *= x
+            if n & 1:
+                r *= x
             x *= x
             n >>= 1
-
         return r
-    
+
     def myPow(self, x, n):
-        print(x,n)
-        if n == 0: return 1
-        elif n < 0: return self.myPow(1/x, -n)
-        elif x == float('inf'): return x
-        elif n % 2: return x * self.myPow( x, n - 1)
-        else: return self.myPow(x * x, n // 2)
+        if n < 0:
+            x = 1 / x
+            n *= -1
+        ans, current_product = 1, x
+        while n > 0:
+            if n % 2 == 1:
+                ans *= current_product
+            current_product *= current_product
+            n //= 2
+        return ans
