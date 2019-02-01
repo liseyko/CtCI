3e0348364c7a727a4a1955a281b83b5852f68819
Alexander Liseyko <liseyko@gmail.com>
2019-01-31 16:18:09 -0800

    modified:   p035 - Search Insert Position.py

--- leetcode/p035 - Search Insert Position.py
+++ leetcode/p035 - Search Insert Position.py
@@ -19,3 +19,14 @@ class Solution:
                 break
 
         return i+1 if target > nums[i] else i
+
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
