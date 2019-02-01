d0598b6b6e703fdb689b42e361da67fa78bcf66a
Alexander Liseyko <liseyko@gmail.com>
2019-01-31 16:14:08 -0800

    modified:   p035 - Search Insert Position.py

--- leetcode/p035 - Search Insert Position.py
+++ leetcode/p035 - Search Insert Position.py
@@ -1,21 +1,21 @@
 class Solution:
-    def binarySearch(self, nums, target, l, r):
-        if r <= l:
-            if target > nums[l]:
-                return l+1
-            return l
-        m = (l + r) // 2
-        if target == nums[m]:
-            return m
-        elif target > nums[m]:
-            return self.binarySearch(nums, target, m+1, r)
-        else:
-            return self.binarySearch(nums, target, l, m-1)
-        
     def searchInsert(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        return self.binarySearch(nums, target, 0, len(nums)-1)
+        j = len(nums)-1
+        i = j if nums and target > nums[-1] else 0
+
+        while i < j:
+            m = i + (j-i) // 2
+            if nums[m] > target:
+                j = m-1
+            elif nums[m] < target:
+                i = m+1
+            else:
+                i = m
+                break
+
+        return i+1 if target > nums[i] else i
