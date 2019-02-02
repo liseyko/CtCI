f19387239d20049c68108a8ebd22c19631866ad4
Alexander Liseyko <liseyko@gmail.com>
2019-02-01 17:58:38 -0800

    modified:   p021 - Merge Two Sorted Lists.py

--- leetcode/p021 - Merge Two Sorted Lists.py
+++ leetcode/p021 - Merge Two Sorted Lists.py
@@ -19,29 +19,3 @@ class Solution:
             cur = cur.next
         cur.next = l1 or l2
         return res.next
-
-    def mergeTwoLists(self, l1, l2):
-        if not (l1 and l2):
-            return l1 or l2
-        if l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwoLists(l2.next, l1)
-            return l2
-
-    def mergeTwoLists(self, l1, l2):
-        if not (l1 and l2):
-            return l1 or l2
-
-        if l2.val < l1.val:
-            l1, l2 = l2, l1
-        head = l1
-
-        while l2:
-            while l1.next and l1.next.val < l2.val:
-                l1 = l1.next
-            l1.next, l2 = l2, l1.next
-            l1 = l1.next
-
-        return head
