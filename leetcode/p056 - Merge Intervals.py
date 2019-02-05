d20eb2b1f8c64276e50c458066faf73bdd3701aa
Alexander Liseyko <liseyko@gmail.com>
2019-02-04 17:23:19 -0800

    modified:   p056 - Merge Intervals.py

--- leetcode/p056 - Merge Intervals.py
+++ leetcode/p056 - Merge Intervals.py
@@ -4,28 +4,27 @@
 #         self.start = s
 #         self.end = e
 
+
 class Solution:
     def merge(self, intervals):
         """
         :type intervals: List[Interval]
         :rtype: List[Interval]
         """
-        intervals.sort(key = lambda x: x.start)
+        intervals.sort(key=lambda x: x.start)
         for i in range(len(intervals) - 1, 0, -1):
-            while i <= len(intervals)-1 and intervals[i-1].end >= intervals[i].start:
+            while i <= len(intervals)-1\
+                  and intervals[i-1].end >= intervals[i].start:
                 intervals[i-1].end = max(intervals[i-1].end, intervals[i].end)
                 del intervals[i]
-                
         return intervals
-    
-    def merge(self, intervals):
+
+    def merge(self, intervals: 'List[Interval]') -> 'List[Interval]':
         intervals.sort(key=lambda x: x.start)
-
-        merged = []
-        for interval in intervals:
-            if not merged or merged[-1].end < interval.start:
-                merged.append(interval)
+        res = [intervals[0]] if intervals else []
+        for i in range(1, len(intervals)):
+            if intervals[i].start <= res[-1].end:
+                res[-1].end = max(res[-1].end, intervals[i].end)
             else:
-                merged[-1].end = max(merged[-1].end, interval.end)
-
-        return merged
+                res.append(intervals[i])
+        return res
