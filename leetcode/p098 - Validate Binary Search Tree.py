15c380021a5bcd0c6206622c311a45838b76704e
Alexander Liseyko <liseyko@gmail.com>
2019-02-06 15:26:50 -0800

    modified:   p098 - Validate Binary Search Tree.py

--- leetcode/p098 - Validate Binary Search Tree.py
+++ leetcode/p098 - Validate Binary Search Tree.py
@@ -7,30 +7,8 @@
 
 
 class Solution:
-    def __init__(self):
-        self.prev = float('-inf')
-
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if not root:
-            return True
-        if not self.isValidBST(root.left) or root.val <= self.prev:
-            return False
-        self.prev = root.val
-        return self.isValidBST(root.right)
-
-    def isValidBST(self, root, miN=float('-inf'), maX=float('inf')):
-        if not root:
-            return True
-        if root.val <= miN or root.val >= maX:
-            return False
-        return self.isValidBST(root.left, miN, min(maX, root.val)) \
-            and self.isValidBST(root.right, max(miN, root.val), maX)
-
-    def isValidBST(self, root, left=float('-inf'), right=float('inf')):
-        return not root or left < root.val < right and \
-                self.isValidBST(root.left, left, root.val) and \
-                self.isValidBST(root.right, root.val, right)
+    def isValidBST(self, root: 'TreeNode',
+                   lt=float('inf'), gt=float('-inf')) -> 'bool':
+        return not root or gt < root.val < lt\
+               and self.isValidBST(root.left, min(root.val, lt), gt)\
+               and self.isValidBST(root.right, lt, max(root.val, gt))
