c566e69594c2fb5d5bce42695e35d4fc3e6a4212
Alexander Liseyko <liseyko@gmail.com>
2019-02-05 17:55:51 -0800

    modified:   p010 - Regular Expression Matching.py

--- leetcode/p010 - Regular Expression Matching.py
+++ leetcode/p010 - Regular Expression Matching.py
@@ -6,7 +6,7 @@ class Solution:
         "ab*a*c*a"
         ---
         "acaabbaccbbacaabbbb"
-        "a*.*b*.*a*aa*a*"     
+        "a*.*b*.*a*aa*a*"
         ---
         "aaaaabaccbbccababa"
         "a*b*.*c*c*.*.*.*c"
@@ -17,45 +17,47 @@ class Solution:
     """
     def __init__(self):
         self.cache = {}
-    # DP top-down solution
+
     def isMatch(self, text, pattern):
+        # DP top-down solution
         memo = {}
+
         def dp(i, j):
-            #print(i,j)
             if (i, j) not in memo:
                 if j == len(pattern):
                     ans = i == len(text)
                 else:
-                    first_match = i < len(text) and pattern[j] in {text[i], '.'}
-                    if j+1 < len(pattern) and pattern[j+1] == '*' and (pattern[j].isalpha() or pattern[j] == '.'):
-                        ans = dp(i, j+2) or first_match and dp(i+1, j)
+                    firstMatch = i < len(text) and pattern[j] in {text[i], '.'}
+                    if j+1 < len(pattern) and pattern[j+1] == '*'\
+                       and (pattern[j].isalpha() or pattern[j] == '.'):
+                        ans = dp(i, j+2) or firstMatch and dp(i+1, j)
                     else:
-                        ans = first_match and dp(i+1, j+1)
+                        ans = firstMatch and dp(i+1, j+1)
 
                 memo[i, j] = ans
             return memo[i, j]
 
         return dp(0, 0)
 
-    # recursive solution + cache
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
-        if not p:
-            return not s
-        
-        if (s, p) in self.cache:
-            return self.cache[(s, p)]
+        self.cache = {}
+        sl, pl = len(s), len(p)
 
-        first_match = bool(s) and p[0] in {s[0], '.'}
+        def _isMatch(si=0, pi=0):
+            if pi == pl:
+                return si == sl
 
-        if len(p) >= 2 and p[1] == '*' and (p[0].isalpha() or p[0] == '.'):
-            self.cache[(s, p)] = (self.isMatch(s, p[2:]) or
-                    first_match and self.isMatch(s[1:], p))
-            return self.cache[(s, p)]
-        else:
-            self.cache[(s, p)] = first_match and self.isMatch(s[1:], p[1:]) 
-            return self.cache[(s, p)]
+            if (si, pi) in self.cache:
+                return self.cache[(si, pi)]
+
+            firstMatch = si < sl and (p[pi] == '.' or p[pi] == s[si])
+
+            if pl - pi > 1 and p[pi+1] == '*':
+                self.cache[(si, pi)] = _isMatch(si, pi+2) or\
+                        firstMatch and _isMatch(si+1, pi)
+                return self.cache[(si, pi)]
+            else:
+                self.cache[(si, pi)] = firstMatch and _isMatch(si+1, pi+1)
+                return self.cache[(si, pi)]
+
+        return _isMatch()
