3a1f698c5d733eb636147fa903c9a5c255bf5eee
Alexander Liseyko <liseyko@gmail.com>
2019-03-09 00:19:00 -0800

    modified:   p038 - Count and Say.py

--- leetcode/p038 - Count and Say.py
+++ leetcode/p038 - Count and Say.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: str
         """
         s0, s1 = ['1'], []
-        for _ in range(1,n):
+        for _ in range(1, n):
             c0, c_cnt = s0[0], 0
             for c1 in s0:
                 if c1 != c0:
-                    s1.extend([str(c_cnt),c0])
+                    s1.extend([str(c_cnt), c0])
                     c0, c_cnt = c1, 1
                 else:
                     c_cnt += 1
-            s1.extend([str(c_cnt),c0])
+            s1.extend([str(c_cnt), c0])
             s0, s1 = s1, []
         return ''.join(s0)
