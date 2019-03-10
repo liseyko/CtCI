54b898bcf12ebed18ee75ff2fa00abd0f115a99a
Alexander Liseyko <liseyko@gmail.com>
2019-03-09 19:33:57 -0800

    modified:   p118 - Pascal's Triangle.py

--- leetcode/p118 - Pascal's Triangle.py
+++ leetcode/p118 - Pascal's Triangle.py
@@ -1,25 +1,23 @@
 class Solution:
-    def __init__(self):
-        self.pt = [[1], [1, 1]]
-
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        while numRows > len(self.pt):
-            tail, newtail = self.pt[-1], [1]
-            tl = len(tail)
-            for i in range(1, tl // 2 + 1):
-                newtail.append(tail[i-1] + tail[i])
-            newtail.extend(newtail[tl // 2 - (tl + 1) % 2::-1])
-            self.pt.append(newtail)
-
-        return self.pt[:numRows]
-
     def generate(self, numRows):
         r = [[1], [1, 1]]
         for n in range(2, numRows):
             r.append([1] + [r[-1][i-1] + r[-1][i] for i in range(1, n)] + [1])
 
         return r[:numRows]
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        self.res = [[1], [1, 1]] 
+        for _ in range(2, numRows):
+            self.res.append(self.generateNextRow())
+            
+        return self.res[:numRows]
+            
+    def generateNextRow(self):
+        lastRow = self.res[-1]
+        newRowMidPart = [lastRow[col-1] + lastRow[col] 
+                         for col in range(1, len(lastRow))]
+        nextRow = [1] + newRowMidPart + [1]
+        return nextRow
