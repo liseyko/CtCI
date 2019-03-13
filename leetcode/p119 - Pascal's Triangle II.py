b294607514773694edb0e023672c85b4bf08d90d
Alexander Liseyko <liseyko@gmail.com>
2019-03-13 13:02:55 -0700

    modified:   p119 - Pascal's Triangle II.py

--- leetcode/p119 - Pascal's Triangle II.py
+++ leetcode/p119 - Pascal's Triangle II.py
@@ -1,26 +1,15 @@
 class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        res = [1, 1]
+        for i in range(2, rowIndex+1):
+            res = [1] + [res[j-1]+res[j] for j in range(1, i)] + [1]
+        return res[:rowIndex+1]
+
     def getRow(self, rowIndex):
-        """
-        :type rowIndex: int
-        :rtype: List[int]
-        """
-        if rowIndex == 0: return [1]
-        row = [1, 1]
-        for _ in range(1, rowIndex):
-            row.append(1)
-            if len(row) % 2:
-                row[len(row) // 2] = row[len(row) // 2] + row[len(row) // 2 - 1]
-            for i in range(len(row) // 2 - 1, 0, -1):
-                row[i] = row[i] + row[i-1]
-                row[-i-1] = row[i]
-
-        return row
-
-    def getRow(self, rowIndex):    
         row = [1]
         for _ in range(rowIndex):
             row = [x + y for x, y in zip([0]+row, row+[0])]
-        return row    
+        return row
 
     def getRow(self, rowIndex):
         row = [1]
@@ -28,5 +17,12 @@ class Solution:
             row.append(1)
             for i in range(k, 0, -1):
                 row[i] = row[i] + row[i-1]
-
         return row
+
+    def getRow(self, rowIndex: int) -> List[int]:
+        res = [1]
+        for i in range(1, rowIndex+1):
+            for j in range(i//2, 0, -1):
+                res[j] = res[i-j] = res[j] + res[j-1]
+            res.append(1)
+        return res
