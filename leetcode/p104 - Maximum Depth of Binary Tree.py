46dc751b0f48fd20aada350e22919e42af2eb5cd
Alexander Liseyko <liseyko@gmail.com>
2019-03-15 14:34:22 -0700

    modified:   p104 - Maximum Depth of Binary Tree.py

--- leetcode/p104 - Maximum Depth of Binary Tree.py
+++ leetcode/p104 - Maximum Depth of Binary Tree.py
@@ -8,13 +8,27 @@ from collections import deque
 
 
 class Solution:
+    """ recursive """
     def maxDepth(self, root: TreeNode) -> int:
         return 1 + max(self.maxDepth(root.left),
                        self.maxDepth(root.right)) if root else 0
 
-    def maxDepth(self, root):
+    def maxDepth(self, root: TreeNode) -> int:
+        """ BFS """
         lvl, depth = [root], 0
         while any(lvl):
             lvl = [c for n in lvl for c in (n.left, n.right) if c]
             depth += 1
+        return depth
+
+    def maxDepth(self, root: TreeNode) -> int:
+        """ DFS """
+        stack = [(1, root)] if root else None
+        depth = 0
+        while stack:
+            cur_depth, cur_node = stack.pop()
+            depth = max(depth, cur_depth)
+            for chld in cur_node.left, cur_node.right:
+                if chld:
+                    stack.append((cur_depth+1, chld))
         return depth
