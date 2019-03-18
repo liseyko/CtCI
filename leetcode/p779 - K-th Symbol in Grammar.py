bac8c4cd406f28af5f149ebfb56daf5ef361ceb3
Alexander Liseyko <liseyko@gmail.com>
2019-03-18 14:17:52 -0700

    modified:   p779 - K-th Symbol in Grammar.py

--- leetcode/p779 - K-th Symbol in Grammar.py
+++ leetcode/p779 - K-th Symbol in Grammar.py
@@ -3,3 +3,13 @@ class Solution:
         if N == 1:
             return 0
         return (1 - K % 2) ^ self.kthGrammar(N-1, (K+1)//2)
+
+    def kthGrammar(self, N, K):
+        if N == 1:
+            return 0
+        if K <= (2**(N-2)):
+            return self.kthGrammar(N-1, K)
+        return self.kthGrammar(N-1, K - 2**(N-2)) ^ 1
+
+    def kthGrammar(self, N, K):
+        return bin(K - 1).count('1') % 2
