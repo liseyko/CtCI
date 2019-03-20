4023ca4630a0a9c4d48717084136ac3a39596d70
Alexander Liseyko <liseyko@gmail.com>
2019-03-20 11:39:28 -0700

    modified:   p096 - Unique Binary Search Trees.py

--- leetcode/p096 - Unique Binary Search Trees.py
+++ leetcode/p096 - Unique Binary Search Trees.py
@@ -1,24 +1,14 @@
 class Solution:
-    def _numTrees(self, n: int) -> int:
-        if n in self.res:
-            return self.res[n]
-        numberOfUniqueBST = 0
-        for i in range(n):
-            numberOfUniqueBST += self._numTrees(i) * self._numTrees(n-1-i)
-        self.res[n] = numberOfUniqueBST
-        return numberOfUniqueBST
-
     def numTrees(self, n: int) -> int:
-        self.res = {0: 1, 1: 1}
-        return self._numTrees(n)
-
-
-class Solution:
-    def numTrees(self, n: int) -> int:
-        res = [1, 1]
+        res = [1, 1] + [0]*(n-1)
         for i in range(2, n+1):
-            res.append(0)
             for j in range(i):
-                res[-1] += res[j] * res[i-1-j]
-
+                res[i] += res[j] * res[i-1-j]
         return res[n]
+
+    def numTrees(self, n):
+        """ Catalan number """
+        C = 1
+        for i in range(0, n):
+            C = C * 2*(2*i+1)/(i+2)
+        return int(C)
