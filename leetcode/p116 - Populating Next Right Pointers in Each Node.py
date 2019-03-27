3aec83cda3d1b28a882e59087a18854e9d158e70
Alexander Liseyko <liseyko@gmail.com>
2019-03-27 15:51:51 -0700

    modified:   p116 - Populating Next Right Pointers in Each Node.py

--- leetcode/p116 - Populating Next Right Pointers in Each Node.py
+++ leetcode/p116 - Populating Next Right Pointers in Each Node.py
@@ -1,24 +1,32 @@
-# Definition for binary tree with next pointer.
-# class TreeLinkNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#         self.next = None
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
 
 
 class Solution:
-    # @param root, a tree link node
-    # @return nothing
-    def connect(self, root):
-        q = collections.deque([root])
-        while any(q):
-            qlen = len(q)
-            while qlen:
-                node = q.popleft()
-                qlen -= 1
-                if qlen:
-                    node.next = q[0]
-                for c in [node.left, node.right]:
-                    if c:
-                        q.append(c)
+    def connect(self, root: 'Node') -> 'Node':
+        lvl = collections.deque([root])
+        while any(lvl):
+            for i in range(1, len(lvl)):
+                lvl[i-1].next = lvl[i]
+            lvl = [c for n in lvl for c in (n.left, n.right) if c]
+        return root
+
+    def connect(self, root: 'Node') -> 'Node':
+        head = node = root if root else Node(0, None, None, None)
+        while head.left:
+            while node:
+                node.left.next = node.right
+                if node.next:
+                    node.right.next = node.next.left
+                node = node.next
+
+            head = node = head.left
+
+        return root
