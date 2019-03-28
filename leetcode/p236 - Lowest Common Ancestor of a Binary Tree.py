c70a8e461c4daaa115e1942c746080775a0f1428
Alexander Liseyko <liseyko@gmail.com>
2019-03-28 12:44:11 -0700

    modified:   p236 - Lowest Common Ancestor of a Binary Tree.py

--- leetcode/p236 - Lowest Common Ancestor of a Binary Tree.py
+++ leetcode/p236 - Lowest Common Ancestor of a Binary Tree.py
@@ -49,46 +49,46 @@ class Solution:
         return self.ans
 
     def lowestCommonAncestor(self, root, p, q):
-            lca = None
-            stk = [(root, [root.right, root.left])]
-            while stk:
-                if len(stk[-1][1]) == 2:  # first encounter
-                    r = stk[-1][0]
-                    if r == p or r == q:
-                        if lca is not None:
+        lca = None
+        stk = [(root, [root.right, root.left])]
+        while stk:
+            if len(stk[-1][1]) == 2:  # first encounter
+                r = stk[-1][0]
+                if r == p or r == q:
+                    if lca is not None:
+                        return stk[lca][0]
+                    else:
+                        lca = len(stk)-1
+            if stk[-1][1]:
+                c = stk[-1][1].pop()
+                if c:
+                    stk.append((c, [c.right, c.left]))
+            else:
+                stk.pop()
+                if lca == len(stk):
+                    lca -= 1
+
+    def lowestCommonAncestor(self, root, p, q):
+        stk, lca = [[root, 2]], -1
+        while stk:
+            n, paths = stk[-1]
+            if paths:
+                if paths == 2:  # first encounter
+                    if n == p or n == q:
+                        if lca >= 0:
                             return stk[lca][0]
                         else:
                             lca = len(stk)-1
-                if stk[-1][1]:
-                    c = stk[-1][1].pop()
-                    if c:
-                        stk.append((c, [c.right, c.left]))
-                else:
-                    stk.pop()
-                    if lca == len(stk):
-                        lca -= 1
-
-    def lowestCommonAncestor(self, root, p, q):
-            stk, lca = [[root, 2]], -1
-            while stk:
-                n, paths = stk[-1]
-                if paths:
-                    if paths == 2:  # first encounter
-                        if n == p or n == q:
-                            if lca >= 0:
-                                return stk[lca][0]
-                            else:
-                                lca = len(stk)-1
-                        c = n.left
-                    elif paths == 1:  # second
-                        c = n.right
-                    stk[-1][1] -= 1
-                    if c:
-                        stk.append([c, 2])
-                else:  # final
-                    stk.pop()
-                    if lca == len(stk):
-                        lca -= 1
+                    c = n.left
+                elif paths == 1:  # second
+                    c = n.right
+                stk[-1][1] -= 1
+                if c:
+                    stk.append([c, 2])
+            else:  # final
+                stk.pop()
+                if lca == len(stk):
+                    lca -= 1
 
     def lowestCommonAncestor(self, root, n1, n2):
         # states:
@@ -97,7 +97,6 @@ class Solution:
         found = False
         lca_idx = -1
         stk = [[root, U]]
-        path = []
         while stk:
             cn, state = stk[-1]
             if state == U:
@@ -118,3 +117,35 @@ class Solution:
                 stk.pop()
                 if found and lca_idx == len(stk):
                     lca_idx -= 1
+
+
+UNPROC, LEFT, BOTH = 0, 1, 2
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = [[root, UNPROC]] if root else None
+        found, result = False, None
+        lookup = {p, q}
+        while stack:
+            node, status = stack[-1]
+
+            if node in lookup:
+                lookup.remove(node)
+                if found:
+                    return result
+                else:
+                    found, result = True, node
+
+            if status == UNPROC:
+                stack[-1][1] = LEFT
+                if node.left:
+                    stack.append([node.left, UNPROC])
+            elif status == LEFT:
+                stack[-1][1] = BOTH
+                if node.right:
+                    stack.append([node.right, UNPROC])
+            elif status == BOTH:
+                stack.pop()
+                if found and result == node:
+                    result = stack[-1][0] if stack else None
