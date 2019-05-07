fc2faa23f4b8caa480b7a9bbae913bb652c7e0c5
Alexander Liseyko <liseyko@gmail.com>
2019-05-07 11:41:55 -0700

    modified:   p066 - Plus One.py

--- leetcode/p066 - Plus One.py
+++ leetcode/p066 - Plus One.py
@@ -4,27 +4,24 @@ class Solution:
         :type digits: List[int]
         :rtype: List[int]
         """
-        r = 0
-        digits[-1] += 1
-        for i in range(len(digits)-1, -1, -1):
-            digits[i] += r
-            r = digits[i] // 10
-            if r == 0:
-                return digits
-            else:
-                digits[i] %= 10
-
-        return [r] + digits
-
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
         carry = 1
         for i, d in enumerate(digits[::-1]):
             carry, digits[~i] = divmod(digits[~i] + carry, 10)
         if carry:
             digits = [carry] + digits
+
+        return digits
+
+    def plusOne(self, digits: List[int]) -> List[int]:
+        curDigitIdx = len(digits) - 1
+        remainder = 1
+
+        while remainder and curDigitIdx > -1:
+            digits[curDigitIdx] += remainder
+            remainder, digits[curDigitIdx] = divmod(digits[curDigitIdx], 10)
+            curDigitIdx -= 1
+
+        if remainder:
+            digits = [remainder] + digits
 
         return digits
