fa1a075dd60d5c6420762364b1e198746c0e7d3f
Alexander Liseyko <liseyko@gmail.com>
2019-05-08 14:24:10 -0700

    modified:   p067 - Add Binary.py

--- leetcode/p067 - Add Binary.py
+++ leetcode/p067 - Add Binary.py
@@ -8,11 +8,11 @@ class Solution:
         if len(a) < len(b):
             a, b = b, a
         maxlen, minlen = len(a), len(b)
-        bits, chars = {'0':0, '1':1}, {0:'0', 1:'1'}
+        bits, chars = {'0': 0, '1': 1}, {0: '0', 1: '1'}
 
         r = list(a)
         leftover = 0
-        
+
         for i in range(maxlen):
             r[~i] = bits[a[~i]] + leftover
             if i < minlen:
@@ -24,6 +24,25 @@ class Solution:
             if leftover:
                 r[~i] %= 2
             r[~i] = chars[r[~i]]
-            
+
         leftover = ["1" for _ in range(leftover)]
         return ''.join(leftover + r)
+
+    def addBinary(self, a: str, b: str) -> str:
+        if len(b) > len(a):
+            a, b = b, a
+
+        result = [int(c) for c in a[::-1]] + [0]
+        carry = 0
+
+        for i in range(len(b)):
+            carry, result[i] = divmod(result[i] + int(b[~i]) + carry, 2)
+        i = len(b)
+        while carry:
+            carry, result[i] = divmod(result[i] + carry, 2)
+            i += 1
+
+        if not result[-1]:
+            result.pop()
+
+        return ''.join(map(str, result[::-1]))
