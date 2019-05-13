93468d385161e27ca1e1e099be0551933fe2878f
Alexander Liseyko <liseyko@gmail.com>
2019-05-13 11:13:04 -0700

    modified:   p088 - Merge Sorted Array.py

--- leetcode/p088 - Merge Sorted Array.py
+++ leetcode/p088 - Merge Sorted Array.py
@@ -14,3 +14,17 @@ class Solution:
             elif n:
                 nums1[m+n-1] = nums2[n-1]
                 n -= 1
+
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        k = m + n - 1
+        m, n = m - 1, n - 1
+        while n >= 0:
+            nums1[k] = max(nums1[m], nums2[n]) if m+1 else nums2[n]
+            k -= 1
+            if m+1 and nums1[m] >= nums2[n]:
+                m -= 1
+            else:
+                n -= 1
