323c90fcac82a737a4a45405b1c4ceb9b71f95ed
Alexander Liseyko <liseyko@gmail.com>
2019-05-14 11:35:07 -0700

    modified:   p122 - Best Time to Buy and Sell Stock II.py

--- leetcode/p122 - Best Time to Buy and Sell Stock II.py
+++ leetcode/p122 - Best Time to Buy and Sell Stock II.py
@@ -1,19 +1,21 @@
 class Solution:
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        
-        maxprofit = 0
-        for i in range(1,len(prices)):
-            if prices[i] > prices[i - 1]:
-                maxprofit += prices[i] - prices[i - 1]
-        
-        return maxprofit
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        curMin = prices[0] if prices else 0
+        for curPrice in prices[1:]:
+            curMin = min(curMin, curPrice)
+            maxProfit += curPrice - curMin
+            curMin = curPrice
+        return maxProfit
+
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                maxProfit += prices[i] - prices[i-1]
+        return maxProfit
         """
         # sol1: peaks and valleys
-        
         if len(prices) < 2: return 0
         profit = 0
         i = 1
@@ -23,11 +25,11 @@ class Solution:
             buy = i-1
             while i < len(prices) - 1 and prices[i+1] >= prices[i]:
                 i += 1
-            
+
             if i < len(prices):
                 profit += prices[i] - prices[buy]
                 print(prices[buy], prices[i])
             i += 1
-        
+
         return profit
         """
