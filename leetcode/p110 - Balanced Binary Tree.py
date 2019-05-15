5cadb581f33344eb7c846f97370774c1bd9d9e34
Alexander Liseyko <liseyko@gmail.com>
2019-05-15 12:06:22 -0700

    modified:   p110 - Balanced Binary Tree.py

--- leetcode/p110 - Balanced Binary Tree.py
+++ leetcode/p110 - Balanced Binary Tree.py
@@ -5,52 +5,42 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def __init__(self):
-        self.balanced = True
+        self.heights = {}
 
     def getHeight(self, root):
-        if not self.balanced:
-            return -1
+        if root in self.heights:
+            return self.heights[root]
         if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-
-        l = self.getHeight(root.left)
-        r = self.getHeight(root.right)
-        if abs(l - r) > 1:
-           self.balanced = False
-
-        return 1 + max(l, r)
-
-
-    def isBalanced(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        self.getHeight(root)
-        return self.balanced
-
-
-class Solution:
-    def __init__(self):
-        self.depths = {}
-    def getDepth(self, root):
-        if root in self.depths:
-            return self.depths[root]
-        if not root:
-            self.depths[root] = 0
+            self.heights[root] = 0
         else:
-            self.depths[root] = 1 + max(self.getDepth(root.left), self.getDepth(root.right))
-        return self.depths[root]
+            self.heights[root] = \
+              1 + max(self.getHeight(root.left), self.getHeight(root.right))
+        return self.heights[root]
 
     def isBalanced(self, root: TreeNode) -> bool:
         if not root:
             return True
         if self.isBalanced(root.left) \
-            and self.isBalanced(root.right) \
-            and abs(self.getDepth(root.left) - self.getDepth(root.right)) < 2:
-                return True
+           and self.isBalanced(root.right) \
+           and abs(self.getHeight(root.left) - self.getHeight(root.right)) < 2:
+            return True
         return False
+
+
+class Solution:
+    def dfsHeight(self, root):
+        if not root or not self.balanced:
+            return 0
+        lHeight = self.dfsHeight(root.left)
+        rHeight = self.dfsHeight(root.right)
+        if abs(lHeight - rHeight) > 1:
+            self.balanced = False
+        return 1 + max(lHeight, rHeight)
+
+    def isBalanced(self, root: TreeNode) -> bool:
+        self.balanced = True
+        self.dfsHeight(root)
+        return self.balanced
