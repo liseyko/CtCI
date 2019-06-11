44c1bf69be182696eae915ce3f420baf289d3fac
Alexander Liseyko <liseyko@gmail.com>
2019-06-11 12:19:59 -0700

    modified:   p046 - Permutations.py

--- leetcode/p046 - Permutations.py
+++ leetcode/p046 - Permutations.py
@@ -1,4 +1,19 @@
-def heaps_algorithm(a, n = None):
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def bt(idxs=[], used=set()):
+            if len(idxs) == len(nums):
+                res.append([nums[j] for j in idxs])
+                return
+            for j in range(len(nums)):
+                if j not in used:
+                    bt(idxs+[j], used | {j})
+        bt()
+        return res
+
+
+def heaps_algorithm(a, n=None):
     if n is None:
         n = len(a)
     if n == 1:  # (got a new permutation)
@@ -8,14 +23,15 @@ def heaps_algorithm(a, n = None):
         heaps_algorithm(a, n-1)
         # always swap the first when odd,
         # swap the i-th when even
-        if n % 2 == 0: 
+        if n % 2 == 0:
             a[n-1], a[i] = a[i], a[n-1]
         else:
             a[n-1], a[0] = a[0], a[n-1]
-    heaps_algorithm(a, n-1);
+    heaps_algorithm(a, n-1)
+
 
 class Solution:
-    def get_perm_idxs(self,idx_set): # 1 2 3
+    def get_perm_idxs(self, idx_set):  # 1 2 3
         if len(idx_set) < 2:
             return [list(idx_set)]
         r = []
@@ -30,7 +46,8 @@ class Solution:
         """
         return self.get_perm_idxs(set(nums))
 
+
 if __name__ == '__main__':
-    heaps_algorithm([1,2,3],3)
+    heaps_algorithm([1, 2, 3], 3)
     s = Solution()
-    print(s.permute([1,2,3]))
+    print(s.permute([1, 2, 3]))
