1bea73d47e4547df7923bdc866b44ecc31b5cbd0
Alexander Liseyko <liseyko@gmail.com>
2019-06-18 16:34:05 -0700

    modified:   p049 - Group Anagrams.py

--- leetcode/p049 - Group Anagrams.py
+++ leetcode/p049 - Group Anagrams.py
@@ -1,24 +1,22 @@
-from collections import defaultdict
 class Solution:
-   
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        res = defaultdict(list)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = collections.defaultdict(list)
         for s in strs:
             res[''.join(sorted(s))].append(s)
-            
+
         return list(res.values())
 
-    def groupAnagrams(self, strs):
-        res = defaultdict(list)
-        cnt_tpl = [0] * 26
-        for s in strs:
-            cnt = cnt_tpl[:]
-            for c in s:
-                cnt[ord(c)-97] += 1
-            res[tuple(cnt)].append(s)
-            
-        return list(res.values())
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        keyTpl = [0]*26
+
+        def mkkey(w):
+            key = keyTpl[:]
+            for c in w:
+                key[ord(c)-ord('a')] += 1
+            return tuple(key)
+
+        dic = collections.defaultdict(list)
+        for word in strs:
+            dic[mkkey(word)].append(word)
+
+        return list(dic.values())
