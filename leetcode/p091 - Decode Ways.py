0462cb0c48548b6a9adb2c20154f5c5a95f928ab
Alexander Liseyko <liseyko@gmail.com>
2019-06-21 10:36:04 -0700

    modified:   p091 - Decode Ways.py

--- leetcode/p091 - Decode Ways.py
+++ leetcode/p091 - Decode Ways.py
@@ -44,3 +44,28 @@ class Solution:
             dp[:] = 0, *dp[:2]
 
         return dp[1] if s else 0
+
+    def numDecodings(self, s: str) -> int:
+        res = collections.deque([1] + [0 if s[0] == "0" else 1])
+        for i in range(1, len(s)):
+            res.append(0)
+            if s[i] != "0":
+                res[-1] += res[-2]
+            if s[i-1] == "1" or s[i-1] == "2" and s[i] < "7":
+                res[-1] += res[0]
+            res.popleft()
+            if not res[-1]:
+                break
+        return res[-1]
+
+    def numDecodings(self, s: str) -> int:
+        res = [0]*len(s) + [1] + [0]
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == "0":
+                if not res[i+1]:
+                    break
+                continue
+            res[i] += res[i+1]
+            if s[i] == "1" or s[i] == "2" and i < len(s)-1 and s[i+1] < "7":
+                res[i] += res[i+2]
+        return res[0]
