ab2e72a53663c6bde2c664ebce30ae5a24412f88
Alexander Liseyko <liseyko@gmail.com>
2019-06-26 10:55:46 -0700

    modified:   p322 - Coin Change.py

--- leetcode/p322 - Coin Change.py
+++ leetcode/p322 - Coin Change.py
@@ -1,32 +1,18 @@
 class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        maxval = 2147483647
-        sol = [0] + [maxval for _ in range(amount)]
-
-        for subval in range(1, amount + 1):
-            for c in coins:
-                if c <= subval:
-                    sol[subval] = min(sol[subval], sol[subval - c] + 1)
-
-        if sol[amount] == maxval:
-            return -1
-        else:
-            return sol[amount]
-
     def coinChange(self, coins: List[int], amount: int) -> int:
-        res = [0] + [amount+1]*amount
 
-        for i in range(1, len(res)):
-            for c in coins:
-                if c <= i:
-                    res[i] = min(res[i], res[i-c]+1)
+        def _coinChange(amount):
+            for coin in coins:
+                if coin > amount:
+                    continue
+                res[amount] = min(res[amount], res[amount-coin] + 1)
 
-        return res[amount] if res[amount] <= amount else -1
+        maxval = amount + 1
+        res = [0] + [maxval] * amount
+        for val in range(1, amount+1):
+            _coinChange(val)
+
+        return res[amount] if res[amount] < maxval else -1
 
     def coinChange(self, coins: List[int], amount: int) -> int:
         res_amt = {}
