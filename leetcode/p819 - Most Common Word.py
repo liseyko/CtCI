80bc32fb82b19e94a1df2af0ad0c6801261e51fd
Alexander Liseyko <liseyko@gmail.com>
2019-07-01 16:34:36 -0700

    modified:   p819 - Most Common Word.py

--- leetcode/p819 - Most Common Word.py
+++ leetcode/p819 - Most Common Word.py
@@ -1,21 +1,19 @@
 import re
+
+
 class Solution:
-    def mostCommonWord(self, paragraph, banned):
-        """
-        :type paragraph: str
-        :type banned: List[str]
-        :rtype: str
-        """
-        wc, banned = {}, set(banned)
-       
-        for w in re.split("[;?!',. ]\s*", paragraph.lower().strip(";?!',. ")):
-            if w in banned: continue
-            if w in wc: wc[w] += 1
-            else: wc[w] = 1
-
-        return sorted(wc.items(), key = lambda x: x[1])[-1][0]
-
     def mostCommonWord(self, p, banned):
         banned = set(banned)
         words = re.findall(r'\w+', p.lower())
         return collections.Counter(w for w in words if w not in banned).most_common(1)[0][0]
+
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        banned = set(banned) | {''}
+        wc = collections.defaultdict(int)
+        topCntWrd = (0, None)
+        for w in re.split('[ ,.;:!?"\']', paragraph.lower()):
+            if w in banned:
+                continue
+            wc[w] += 1
+            topCntWrd = max(topCntWrd, (wc[w], w))
+        return topCntWrd[1]
