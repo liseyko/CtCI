b84423d08970dd7306bf884eb1c780101ef8a7f1
Alexander Liseyko <liseyko@gmail.com>
2019-07-02 15:32:03 -0700

    modified:   p224 - Basic Calculator.py

--- leetcode/p224 - Basic Calculator.py
+++ leetcode/p224 - Basic Calculator.py
@@ -22,3 +22,24 @@ class Solution:
                     i += 1
             return res, i
         return eval()[0]
+
+    def calculate(self, s):
+        ops = set(['-', '+'])
+        res, num, sign, stack = 0, 0, 1, []
+        for c in s:
+            if c.isdigit():
+                num = 10*num + int(c)
+            elif c in ops:
+                res += sign*num
+                num = 0
+                sign = [-1, 1][c == "+"]
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                sign, res = 1, 0
+            elif c == ")":
+                res += sign*num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res + num*sign
