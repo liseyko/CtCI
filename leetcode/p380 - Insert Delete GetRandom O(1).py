d517c9606624e0641664fcc5c2cdcf2786b7ca67
Alexander Liseyko <liseyko@gmail.com>
2019-07-10 12:35:19 -0700

    modified:   p380 - Insert Delete GetRandom O(1).py

--- leetcode/p380 - Insert Delete GetRandom O(1).py
+++ leetcode/p380 - Insert Delete GetRandom O(1).py
@@ -1,31 +1,23 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.val2id = {}
-        self.id2val = {}
+        self.dvk, self.data = {}, []  # dataVal2Key, dataList
 
     def insert(self, val: int) -> bool:
-        if val in self.val2id:
+        if val in self.dvk:
             return False
-        cur_id = len(self.val2id)
-        self.val2id[val] = cur_id
-        self.id2val[cur_id] = val
+        self.dvk[val] = len(self.data)
+        self.data.append(val)
         return True
 
     def remove(self, val: int) -> bool:
-        if val in self.val2id:
-            last_id = len(self.val2id)-1
-            last_val = self.id2val[last_id]
-            cur_id = self.val2id[val]
-            self.val2id[last_val] = cur_id
-            self.id2val[cur_id] = last_val
-            del self.val2id[val]
-            del self.id2val[last_id]
-            return True
-        return False
+        if val not in self.dvk:
+            return False
+        self.data[self.dvk[val]] = self.data[-1]
+        self.dvk[self.data[-1]] = self.dvk[val]
+        self.data.pop()
+        del self.dvk[val]
+        return True
 
     def getRandom(self) -> int:
-        max_id = len(self.val2id)-1
-        if max_id >= 0:
-            id = random.randint(0, max_id)
-            return self.id2val[id]
+        return random.choice(self.data)
