a7f5c6f6f7b5912e820da314f3050954180cfe10
Alexander Liseyko <liseyko@gmail.com>
2019-07-11 12:14:07 -0700

    modified:   p146 - LRU Cache.py

--- leetcode/p146 - LRU Cache.py
+++ leetcode/p146 - LRU Cache.py
@@ -1,11 +1,57 @@
 class Node:
     def __init__(self, val):
-        self.next = None
-        self.prev = None
+        self.prev, self.next = None, None
         self.val = val
 
+    def pop(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+        return self
 
 class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.kv, self.kn = {}, {}  # key: val, key: node
+        self.head, self.tail = Node('^'), Node('$')
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.kv:
+            return -1
+        self.refresh(key)
+        return self.kv[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.kv[key] = value
+        self.refresh(key)
+
+    def refresh(self, key: int) -> None:
+        if key in self.kn:
+            n = self.kn[key].pop()
+        else:
+            n = self.kn[key] = Node(key)
+        self.appendleft(n)
+        self.trim()
+
+    def appendleft(self, n):
+        n.next, n.prev = self.head.next, self.head
+        self.head.next = n.next.prev = n
+
+    def trim(self):
+        if len(self.kn) > self.cap:
+            key = self.tail.prev.pop().val
+            del self.kv[key]
+            del self.kn[key]
+
+class Node:
+    def __init__(self, val):
+        self.next, self.prev = None, None
+        self.val = val
+
+class LRUCache:
+    """ First Attempt """
     def __init__(self, capacity: int):
         self.cap = capacity
         self.head = self.tail = None
