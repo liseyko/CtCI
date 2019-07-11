baac9648daaf5206afca240f4923cd7d0bf13c8e
Alexander Liseyko <liseyko@gmail.com>
2019-07-11 16:38:20 -0700

    modified:   p146 - LRU Cache.py

--- leetcode/p146 - LRU Cache.py
+++ leetcode/p146 - LRU Cache.py
@@ -1,4 +1,54 @@
 class Node:
+    def __init__(self, key=None, val=None):
+        self.prev, self.next = None, None
+        self.key, self.val = key, val
+
+    def pop(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+        return self
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.nodes = {}
+        self.head, self.tail = Node(), Node()
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.nodes:
+            return -1
+        self._refresh(key)
+        return self.nodes[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.nodes:
+            self.nodes[key].val = value
+            self._refresh(key)
+        else:
+            self.nodes[key] = Node(key, value)
+            self._refresh(key, newNode=True)
+
+    def _refresh(self, key: int, newNode=False) -> None:
+        if newNode:
+            self._popright()
+        else:
+            self.nodes[key].pop()
+        self._appendleft(self.nodes[key])
+
+    def _appendleft(self, n: Node) -> None:
+        n.next, n.prev = self.head.next, self.head
+        self.head.next = n.next.prev = n
+
+    def _popright(self) -> None:
+        if len(self.nodes) > self.cap:
+            del self.nodes[self.tail.prev.pop().key]
+
+
+class Node:
     def __init__(self, val):
         self.prev, self.next = None, None
         self.val = val
@@ -45,53 +95,3 @@ class LRUCache:
             key = self.tail.prev.pop().val
             del self.kv[key]
             del self.kn[key]
-
-
-class Node:
-    def __init__(self, val):
-        self.next, self.prev = None, None
-        self.val = val
-
-
-class LRUCache:
-    """ First Attempt """
-    def __init__(self, capacity: int):
-        self.cap = capacity
-        self.head = self.tail = None
-        self.data = {}
-        self.ll = {}
-
-    def upd(self, key):
-        if key not in self.ll:
-            self.ll[key] = Node(key)
-        if self.ll[key] == self.head:
-            return
-        if not self.head:
-            self.head = self.tail = self.ll[key]
-            return
-        if self.ll[key].prev:
-            self.ll[key].prev.next = self.ll[key].next
-            if self.ll[key].next:
-                self.ll[key].next.prev = self.ll[key].prev
-        if self.ll[key] == self.tail:
-            self.tail = self.ll[key].prev
-        if self.head:
-            self.head.prev = self.ll[key]
-        self.ll[key].next = self.head
-        self.head = self.ll[key]
-        if len(self.ll) > self.cap:
-            key = self.tail.val
-            self.tail = self.tail.prev
-            self.tail.next = None
-            del self.data[key]
-            del self.ll[key]
-
-    def get(self, key: int) -> int:
-        if key not in self.data:
-            return -1
-        self.upd(key)
-        return self.data[key]
-
-    def put(self, key: int, value: int) -> None:
-        self.data[key] = value
-        self.upd(key)
