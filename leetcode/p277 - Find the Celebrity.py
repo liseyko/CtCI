8f28411d63f8459348af4ecd20bfc9b06431f859
Alexander Liseyko <liseyko@gmail.com>
2019-07-13 23:40:26 -0700

    modified:   p277 - Find the Celebrity.py

--- leetcode/p277 - Find the Celebrity.py
+++ leetcode/p277 - Find the Celebrity.py
@@ -4,6 +4,38 @@
 # @return a boolean, whether a knows b
 # def knows(a, b):
 
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        celebrity = 0
+        for person in range(1, n):
+            if knows(celebrity, person):
+                celebrity = person
+        if any(knows(celebrity, person) for person in range(celebrity)):
+            return -1
+        if any(not knows(person, celebrity) for person in range(n)):
+            return -1
+
+        return celebrity
+
+    def findCelebrity(self, n):
+        potentialCelebrity = 0
+        for person in range(1, n):
+            if knows(potentialCelebrity, person):
+                potentialCelebrity = person
+        for person in range(potentialCelebrity):
+            if knows(potentialCelebrity, person):
+                return -1
+        for person in range(n):
+            if not knows(person, potentialCelebrity):
+                return -1
+        return potentialCelebrity
+
+
 class Solution(object):
     def findCelebrity(self, n):
         """
