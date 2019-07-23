277f9519f077f9f4a523661e2ec4e7eb93d8dfbc
Alexander Liseyko <liseyko@gmail.com>
2019-07-23 13:24:29 -0700

    modified:   p706 - Design HashMap.py

--- leetcode/p706 - Design HashMap.py
+++ leetcode/p706 - Design HashMap.py
@@ -2,7 +2,8 @@ class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
         self.next = None
-        
+
+
 class MyHashMap:
 
     def __init__(self):
@@ -11,9 +12,9 @@ class MyHashMap:
         """
         self.size = 10000
         self.data = [None] * self.size
-     
+
     def hash(self, key):
-        return ((13 * key + 50000) % 99991 ) % self.size
+        return ((13 * key + 50000) % 99991) % self.size
 
     def put(self, key, value):
         """
@@ -35,11 +36,6 @@ class MyHashMap:
             tail.next = Node(key, value)
 
     def get(self, key):
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        :type key: int
-        :rtype: int
-        """
         hashkey = self.hash(key)
         node = self.data[hashkey]
         while node:
@@ -47,13 +43,8 @@ class MyHashMap:
                 return node.val
             node = node.next
         return -1
-    
+
     def remove(self, key):
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        :type key: int
-        :rtype: void
-        """
         hashkey = self.hash(key)
         node = self.data[hashkey]
         if node:
@@ -66,3 +57,51 @@ class MyHashMap:
                         prv.next = cur.next
                         return
                     prv, cur = cur, cur.next
+
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.k = 101
+        self.buckets = [[] for _ in range(self.k)]
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        b, i = self._getBucketAndIdx(key)
+        if i is not None:
+            b[i][1] = value
+        else:
+            b.append([key, value])
+
+    def _getBucket(self, key):
+        return self.buckets[key % self.k]
+
+    def _getBucketAndIdx(self, key):
+        b = self._getBucket(key)
+        for i, kv in enumerate(b):
+            if kv[0] == key:
+                return b, i
+        return b, None
+
+    def get(self, key: int) -> int:
+        b, i = self._getBucketAndIdx(key)
+        if i is not None:
+            return b[i][1]
+        return -1
+
+    def remove(self, key: int) -> None:
+        b, i = self._getBucketAndIdx(key)
+        if i is not None:
+            del b[i]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
