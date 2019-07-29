28b81ff6333800df0e22abe92ce31365f0599ead
Alexander Liseyko <liseyko@gmail.com>
2019-07-29 13:38:33 -0700

    modified:   p225 - Implement Stack using Queues.py

--- leetcode/p225 - Implement Stack using Queues.py
+++ leetcode/p225 - Implement Stack using Queues.py
@@ -1,54 +1,37 @@
-from collections import deque
 class MyStack:
-
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.q1, self.q2 = deque(), deque()
-        self.top_element = None
+        self.q1 = collections.deque()
+        self.q2 = collections.deque()
 
-    def push(self, x):
+    def push(self, x: int) -> None:
         """
         Push element x onto stack.
-        :type x: int
-        :rtype: void
         """
-        self.top_element = x
+        self.q1, self.q2 = self.q2, self.q1
         self.q1.appendleft(x)
-        
+        while self.q2:
+            self.q1.appendleft(self.q2.pop())
 
-    def pop(self):
+    def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
-        :rtype: int
         """
-        i = 0
-        while self.q1:
-            res = self.q1.pop()
-            if self.q1:
-                self.top_element = res
-                self.q2.appendleft(res)
-        self.q1, self.q2 = self.q2, deque()
-        if not self.q1: 
-            self.top_element = None
-        return res
+        return self.q1.pop()
 
-    def top(self):
+    def top(self) -> int:
         """
         Get the top element.
-        :rtype: int
         """
-        return self.top_element
-        
+        return self.q1[-1]
 
-    def empty(self):
+    def empty(self) -> bool:
         """
         Returns whether the stack is empty.
-        :rtype: bool
         """
-        return len(self.q1) == 0
-
+        return not self.q1
 
 
 # Your MyStack object will be instantiated and called as such:
