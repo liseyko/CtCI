033ddc37aa8598483202309c643b91805ea42cf7
Alexander Liseyko <liseyko@gmail.com>
2019-07-29 13:43:14 -0700

    modified:   p225 - Implement Stack using Queues.py

--- leetcode/p225 - Implement Stack using Queues.py
+++ leetcode/p225 - Implement Stack using Queues.py
@@ -1,37 +1,36 @@
 class MyStack:
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.q1 = collections.deque()
-        self.q2 = collections.deque()
+        self.q = collections.deque()
 
     def push(self, x: int) -> None:
         """
         Push element x onto stack.
         """
-        self.q1, self.q2 = self.q2, self.q1
-        self.q1.appendleft(x)
-        while self.q2:
-            self.q1.appendleft(self.q2.pop())
+        self.q.appendleft(x)
+        for _ in range(len(self.q)-1):
+            self.q.appendleft(self.q.pop())
 
     def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
         """
-        return self.q1.pop()
+        return self.q.pop()
 
     def top(self) -> int:
         """
         Get the top element.
         """
-        return self.q1[-1]
+        return self.q[-1]
 
     def empty(self) -> bool:
         """
         Returns whether the stack is empty.
         """
-        return not self.q1
+        return not self.q
 
 
 # Your MyStack object will be instantiated and called as such:
