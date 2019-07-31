fba12ea8f9290f6bf0e6d5f599fe847cd088dd7d
Alexander Liseyko <liseyko@gmail.com>
2019-07-31 10:35:11 -0700

    modified:   p283 - Move Zeroes.py

--- leetcode/p283 - Move Zeroes.py
+++ leetcode/p283 - Move Zeroes.py
@@ -1,17 +1,4 @@
 class Solution:
-
-    def moveZeroes(self, nums):
-        i = 0
-
-        while i < len(nums) and nums[i] != 0: i += 1
-        
-        for j in range(i+1, len(nums)):
-            if nums[j] != 0:
-                nums[i], i = nums[j], i + 1
-                
-        while i < len(nums):
-            nums[i], i = 0, i + 1
-
     def moveZeroes(self, nums):
         offset = 0
         for i in range(len(nums)):
@@ -19,3 +6,12 @@ class Solution:
                 offset += 1
             elif offset > 0:
                 nums[i - offset], nums[i] = nums[i], 0
+
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = 0  # records the position of "0"
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                continue
+            if i != j:
+                nums[i], nums[j] = nums[j], nums[i]
+            j += 1
