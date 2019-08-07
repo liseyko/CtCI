1c62993f9b6d17537d35ff231eae4195416e474b
Alexander Liseyko <liseyko@gmail.com>
2019-08-07 15:38:27 -0700

    modified:   p155 - Min Stack.py

--- leetcode/p155 - Min Stack.py
+++ leetcode/p155 - Min Stack.py
@@ -1,27 +1,6 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.cmin = float('inf')
-
-    def push(self, x: int) -> None:
-        self.cmin = min(self.cmin, x)
-        self.stack.append((x, self.cmin))
-
-    def pop(self) -> None:
-        self.stack.pop()
-        self.cmin = self.stack[-1][1] if self.stack else float('inf')
-
-    def top(self) -> int:
-        return self.stack[-1][0]
-
-    def getMin(self) -> int:
-        return self.stack[-1][1]
-
-
-class MinStack:
-
-    def __init__(self):
         self.stack = [(None, float('inf'))]
 
     def push(self, x: int) -> None:
