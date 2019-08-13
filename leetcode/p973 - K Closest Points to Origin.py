37326c9b9b9445cd8b15460193d119fccbb780e3
Alexander Liseyko <liseyko@gmail.com>
2019-08-12 17:19:13 -0700

    new file:   p973 - K Closest Points to Origin.py

---
+++ leetcode/p973 - K Closest Points to Origin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        h = [(float('-inf'), None)] * K
+
+        for p in points:
+            dist = p[0]**2 + p[1]**2  # math.sqrt(p[0]**2 + [1]**2)
+            heapq.heappushpop(h, (-dist, p))
+
+        return [p for _, p in h]
