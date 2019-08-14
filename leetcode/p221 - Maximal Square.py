2ff7f06df6c61bf48a6b08d1ee27c64b53daea0f
Alexander Liseyko <liseyko@gmail.com>
2019-08-14 11:39:19 -0700

    modified:   p221 - Maximal Square.py

--- leetcode/p221 - Maximal Square.py
+++ leetcode/p221 - Maximal Square.py
@@ -1,5 +1,43 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        prevRowSq = [0] * (len(matrix[0]) if matrix else 0 + 1)
+        maxSq = 0
+        for row in matrix:
+            curRowSq = [0] * (len(row)+1)
+            for i in range(len(row)):
+                if row[i] == '1':
+                    curRowSq[i] = min(curRowSq[i-1], prevRowSq[i-1], prevRowSq[i])+1
+                    maxSq = max(maxSq, curRowSq[i])
+            prevRowSq = curRowSq
+        return maxSq**2
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        prevRowSq = [0] * (len(matrix[0]) if matrix else 0 + 1)
+        maxSq = 0
+
+        def updCurSq(i, curRowSq):
+            nonlocal prevRowSq, maxSq
+            curRowSq[i] = min(curRowSq[i-1], prevRowSq[i-1], prevRowSq[i])+1
+            maxSq = max(maxSq, curRowSq[i])
+
+        def updSqrsInRow(row):
+            nonlocal prevRowSq
+            curRowSq = [0] * (len(row)+1)
+            for i in range(len(row)):
+                if row[i] == '1':
+                    updCurSq(i, curRowSq)
+            prevRowSq = curRowSq
+
+        for row in matrix:
+            updSqrsInRow(row)
+
+        return maxSq**2
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
         curMax = wi = hi = 0
         h, w = len(matrix), len(matrix[0]) if matrix else 0
 
