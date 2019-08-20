4229238008626845021d193ca247ac262843b28a
Alexander Liseyko <liseyko@gmail.com>
2019-08-20 13:01:40 -0700

    modified:   p348 - Design Tic-Tac-Toe.py

--- leetcode/p348 - Design Tic-Tac-Toe.py
+++ leetcode/p348 - Design Tic-Tac-Toe.py
@@ -1,47 +1,43 @@
+class TicTacToe(object):
+
+    def __init__(self, n):
+        self.n = n
+        self.row, self.col = [0] * n, [0] * n
+        self.diagLR = self.diagRL = 0
+
+    def move(self, row, col, player):
+        offset = player * 2 - 3
+        self.row[row] += offset
+        self.col[col] += offset
+        if row == col:
+            self.diagLR += offset
+        if row == self.n - 1 - col:
+            self.diagRL += offset
+        if offset * self.n in [self.row[row], self.col[col], self.diagLR, self.diagRL]:
+            return player
+
+        return 0
+
+
 class TicTacToe:
 
     def __init__(self, n: int):
-        """
-        Initialize your data structure here.
-        """
         self.n = n
         self.rows = [[0, 0] for _ in range(n)]
         self.cols = [[0, 0] for _ in range(n)]
         self.diag1, self.diag2 = [0, 0], [0, 0]
 
     def move(self, row: int, col: int, player: int) -> int:
-        """
-        Player {player} makes a move at ({row}, {col}).
-        @param row The row of the board.
-        @param col The column of the board.
-        @param player The player, can be either 1 or 2.
-        @return The current winning condition, can be either:
-                0: No one wins.
-                1: Player 1 wins.
-                2: Player 2 wins.
-        """
-
-        self.rows[col][player-1] += 1
-        if self.rows[col][player-1] == self.n:
-            return player
-
-        self.cols[row][player-1] += 1
-        if self.cols[row][player-1] == self.n:
-            return player
-
+        player -= 1
+        self.rows[col][player] += 1
+        self.cols[row][player] += 1
         if row == col:
-            self.diag1[player-1] += 1
-            if self.diag1[player-1] == self.n:
-                return player
-
+            self.diag1[player] += 1
         if row == self.n-1-col:
-            self.diag2[player-1] += 1
-            if self.diag2[player-1] == self.n:
-                return player
+            self.diag2[player] += 1
+
+        if self.n in (self.rows[col][player], self.cols[row][player],
+                      self.diag1[player], self.diag2[player]):
+            return player+1
 
         return 0
-
-
-# Your TicTacToe object will be instantiated and called as such:
-# obj = TicTacToe(n)
-# param_1 = obj.move(row,col,player)
