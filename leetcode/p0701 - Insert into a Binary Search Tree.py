f4af4bbe6f4c7a5fc9b3c2fb705484e86e3c0cb5
Alexander Liseyko <liseyko@gmail.com>
2019-08-23 16:56:19 -0700

    modified:   p0701 - Insert into a Binary Search Tree.py

--- leetcode/p0701 - Insert into a Binary Search Tree.py
+++ leetcode/p0701 - Insert into a Binary Search Tree.py
@@ -8,16 +8,14 @@
 
 class Solution:
     def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
-        n = root
+        n, newNode = root, TreeNode(val)
         while n:
             if val > n.val:
                 p, n, loc = n, n.right, 'right'
             else:
                 p, n, loc = n, n.left, 'left'
-
         if root:
-            setattr(p, loc, TreeNode(val))
+            setattr(p, loc, newNode)
         else:
-            root = TreeNode(val)
-
+            root = newNode
         return root
