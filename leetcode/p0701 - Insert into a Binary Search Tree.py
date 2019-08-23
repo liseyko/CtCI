fd992c67e379bbe3ae69491eb0feeb7af18845f6
Alexander Liseyko <liseyko@gmail.com>
2019-08-23 15:43:44 -0700

    new file:   p0701 - Insert into a Binary Search Tree.py

---
+++ leetcode/p0701 - Insert into a Binary Search Tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        def find(r = root, p = None, v = val):
+            if not r:
+                return p
+            if v > r.val:
+                return find(r.right, r)
+            elif v < r.val:
+                return find(r.left, r)
+
+        if not root:
+            return TreeNode(val)
+        n = TreeNode(val)
+        p = find()
+
+        if n.val > p.val:
+            n.right, p.right = p.right, n
+        else:
+            n.left, p.left = p.left, n
+
+        return root
