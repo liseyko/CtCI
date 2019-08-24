f5f9ffdea95df11dff043ec307b6cc2e1dd0c242
Alexander Liseyko <liseyko@gmail.com>
2019-08-23 18:04:49 -0700

    modified:   p1021 - Remove Outermost Parentheses.py

--- leetcode/p1021 - Remove Outermost Parentheses.py
+++ leetcode/p1021 - Remove Outermost Parentheses.py
@@ -12,3 +12,14 @@ class Solution:
                     r.extend(buf[1:-1])
                     buf.clear()
         return ''.join(r)
+
+    def removeOuterParentheses(self, S: str) -> str:
+        cnt, res = 0, []
+        for c in S:
+            if c == ')':
+                cnt -= 1
+            if cnt != 0:
+                res += [c]
+            if c == '(':
+                cnt+=1
+        return ''.join(res)
