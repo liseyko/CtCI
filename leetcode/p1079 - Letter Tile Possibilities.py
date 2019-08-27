97bf06823df6fb19d80226fa559a00f7fa617ecb
Alexander Liseyko <liseyko@gmail.com>
2019-08-27 16:18:26 -0700

    modified:   p1079 - Letter Tile Possibilities.py

--- leetcode/p1079 - Letter Tile Possibilities.py
+++ leetcode/p1079 - Letter Tile Possibilities.py
@@ -10,3 +10,19 @@ class Solution:
                 bt(buf+[t], tiles[:i]+tiles[i+1:])
         bt()
         return len(res)
+
+    def numTilePossibilities(self, tiles: str) -> int:
+        t = collections.defaultdict(int)
+        for c in tiles:
+            t[c] += 1
+        res = -1
+        def bt():
+            nonlocal res
+            res += 1
+            for key in t:
+                if t[key]:
+                    t[key] -= 1
+                    bt()
+                    t[key] += 1
+        bt()
+        return res
