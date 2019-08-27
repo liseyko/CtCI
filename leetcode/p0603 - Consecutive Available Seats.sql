f7cc870128e56e2d2f887e9ad32f038881a6d96a
Alexander Liseyko <liseyko@gmail.com>
2019-08-27 13:51:34 -0700

    modified:   p0603 - Consecutive Available Seats.sql

--- leetcode/p0603 - Consecutive Available Seats.sql
+++ leetcode/p0603 - Consecutive Available Seats.sql
@@ -1,9 +1,9 @@
 select seat_id
 from cinema
-where (seat_id-1 in 
+where (seat_id-1 in
         (select seat_id
          from cinema
-         where free  = 1) 
+         where free  = 1)
       or seat_id+1 in 
         (select seat_id
         from cinema
@@ -12,6 +12,6 @@ where (seat_id-1 in
 
 select distinct c1.seat_id
 from cinema c1 join cinema c2 
-  on abs(c1.seat_id - c2.seat_id) = 1 
+  on abs(c1.seat_id - c2.seat_id) = 1
   and c1.free = 1 and c2.free = 1
 order by c1.seat_id
