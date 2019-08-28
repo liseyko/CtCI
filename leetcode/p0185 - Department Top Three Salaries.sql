f9965354d51bfe6fbf988a0a733e6d442f6825e9
Alexander Liseyko <liseyko@gmail.com>
2019-08-28 14:24:52 -0700

    modified:   p0185 - Department Top Three Salaries.sql

--- leetcode/p0185 - Department Top Three Salaries.sql
+++ leetcode/p0185 - Department Top Three Salaries.sql
@@ -4,4 +4,14 @@ FROM Employee e1
   LEFT JOIN Employee e2 ON e1.DepartmentId = e2.DepartmentId AND e1.salary <= e2.salary
 group by e1.Id
 having count(distinct e2.Salary) <= 3
-order by e1.DepartmentId, e1.salary DESC
+order by e1.DepartmentId, e1.salary DESC;
+
+select d.Name Department, e1.Name Employee, e1.Salary
+from Employee e1 
+join Department d
+on e1.DepartmentId = d.Id
+where 3 > (select count(distinct(e2.Salary)) 
+                  from Employee e2 
+                  where e2.Salary > e1.Salary 
+                  and e1.DepartmentId = e2.DepartmentId
+                  );
