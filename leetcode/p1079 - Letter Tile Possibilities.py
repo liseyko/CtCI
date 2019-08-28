6704fc507ebe69b0b33ae8a598a217ea87405e6b
Alexander Liseyko <liseyko@gmail.com>
2019-08-27 21:38:21 -0700

    modified:   p1079 - Letter Tile Possibilities.py

--- leetcode/p1079 - Letter Tile Possibilities.py
+++ leetcode/p1079 - Letter Tile Possibilities.py
@@ -32,5 +32,4 @@ class Solution:
         res = {''}
         for c in tiles:
             res |= {d[:i] + c + d[i:] for d in res for i in range(len(d) + 1)}
-            print(res)
         return len(res) - 1
