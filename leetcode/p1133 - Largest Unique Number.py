b3ee06156a337ecca6b9f56a9fd0cb0711837dbf
Alexander Liseyko <liseyko@gmail.com>
2019-08-28 15:56:07 -0700

    modified:   p1133 - Largest Unique Number.py

--- leetcode/p1133 - Largest Unique Number.py
+++ leetcode/p1133 - Largest Unique Number.py
@@ -8,3 +8,11 @@ class Solution:
                 return n
             prev = n
         return A[0] if len(A) == 1 else -1
+
+    def largestUniqueNumber(self, A: List[int]) -> int:
+        da = collections.Counter(A)
+        res = -1
+        for k, v in da.items():
+            if v == 1 and k > res:
+                res = k
+        return res
