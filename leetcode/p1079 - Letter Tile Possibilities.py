002318f20c68bb9de2d721b6401cf58f20886c97
Alexander Liseyko <liseyko@gmail.com>
2019-08-27 21:37:36 -0700

    modified:   p1079 - Letter Tile Possibilities.py

--- leetcode/p1079 - Letter Tile Possibilities.py
+++ leetcode/p1079 - Letter Tile Possibilities.py
@@ -27,3 +27,10 @@ class Solution:
             return n
 
         return bt()
+
+    def numTilePossibilities(self, tiles: str) -> int:
+        res = {''}
+        for c in tiles:
+            res |= {d[:i] + c + d[i:] for d in res for i in range(len(d) + 1)}
+            print(res)
+        return len(res) - 1
