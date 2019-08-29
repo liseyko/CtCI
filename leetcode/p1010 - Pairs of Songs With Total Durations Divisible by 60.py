2c4eaa1fb86a4884dc8e4635c05d66abc7d30670
Alexander Liseyko <liseyko@gmail.com>
2019-08-28 22:38:50 -0700

    modified:   p1010 - Pairs of Songs With Total Durations Divisible by 60.py

--- leetcode/p1010 - Pairs of Songs With Total Durations Divisible by 60.py
+++ leetcode/p1010 - Pairs of Songs With Total Durations Divisible by 60.py
@@ -3,6 +3,15 @@ class Solution:
         res = 0
         a = [0]*60
         for t in time:
-            res += a[(60 - t%60) %60]
-            a[t%60] += 1
+            res += a[(60 - t % 60) % 60]
+            a[t % 60] += 1
+        return res
+
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        res = 0
+        d = collections.defaultdict(int)
+        for t in time:
+            matchingPair = (60 - t % 60) % 60
+            res += d[matchingPair]
+            d[t % 60] += 1
         return res
