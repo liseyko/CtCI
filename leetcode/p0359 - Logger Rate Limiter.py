76783cf5c2afddcddc6f5761cf76368b6b363da9
Alexander Liseyko <liseyko@gmail.com>
2019-08-29 16:29:21 -0700

    modified:   p0359 - Logger Rate Limiter.py

--- leetcode/p0359 - Logger Rate Limiter.py
+++ leetcode/p0359 - Logger Rate Limiter.py
@@ -1,7 +1,6 @@
 class Logger:
-
     def __init__(self):
-        self.d = {}
+        self.ts = {}
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         """
@@ -9,21 +8,33 @@ class Logger:
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity.
         """
-        shouldPrint = message not in self.d or timestamp - self.d[message] >= 10
-        if shouldPrint:
-            self.d[message] = timestamp
-        return shouldPrint
-
-# Your Logger object will be instantiated and called as such:
-# obj = Logger()
-# param_1 = obj.shouldPrintMessage(timestamp,message)
-
-class Logger:
-    def __init__(self):
-        self.ts = {}
-
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         if timestamp < self.ts.get(message, 0):
             return False
         self.ts[message] = timestamp + 10
         return True
+
+
+class Logger:
+
+    def __init__(self):
+        self.ts = {}
+        self.h = []
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+
+        def disablePrintingMsgUntil(n):
+            self.ts[message] = n
+            heapq.heappush(self.h, (n, message))
+
+        def purgeStaleMsgBlocks():
+            while self.h and self.h[0][0] < timestamp:
+                oldest_msg = heapq.heappop(self.h)[1]
+                if self.ts[oldest_msg] < timestamp:
+                    del self.ts[oldest_msg]
+
+        shouldBePrinted = timestamp > self.ts.get(message, -1)
+        if shouldBePrinted:
+            disablePrintingMsgUntil(timestamp+9)
+            purgeStaleMsgBlocks()
+
+        return shouldBePrinted
