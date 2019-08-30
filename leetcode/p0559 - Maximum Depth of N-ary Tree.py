ebc51ec2b6d40580b64928de5a40a136a24ff636
Alexander Liseyko <liseyko@gmail.com>
2019-08-29 23:08:21 -0700

    modified:   p0559 - Maximum Depth of N-ary Tree.py

--- leetcode/p0559 - Maximum Depth of N-ary Tree.py
+++ leetcode/p0559 - Maximum Depth of N-ary Tree.py
@@ -3,3 +3,16 @@ class Solution:
         if not root:
             return 0
         return 1+max([0]+[self.maxDepth(c) for c in root.children])
+
+    def maxDepth(self, root: 'Node') -> int:
+        stack = [(1, root)] if root else None
+        maxdepth = 0
+
+        while stack:
+            depth, node = stack.pop()
+            if node:
+                maxdepth = max(maxdepth, depth)
+                for c in node.children:
+                    stack.append((depth + 1, c))
+
+        return maxdepth
