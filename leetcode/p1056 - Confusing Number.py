b144f09c9de56b1ce13be159a853c31343016aaa
Alexander Liseyko <liseyko@gmail.com>
2019-08-30 16:07:53 -0700

    modified:   p1056 - Confusing Number.py

--- leetcode/p1056 - Confusing Number.py
+++ leetcode/p1056 - Confusing Number.py
@@ -1,10 +1,10 @@
 class Solution:
     def confusingNumber(self, n: int) -> bool:
-        rotated = [0,1,None,None,None,None,9,None,8,6]
+        rotatable_digits  = {0: 0, 1: 1, 6: 9, 8: 8, 9: 6}
         ncopy, n180 = n, 0
         while ncopy:
             ncopy, digit = divmod(ncopy, 10)
-            if rotated[digit] is None:
+            if digit not in rotatable_digits:
                 return False
-            n180 = n180 * 10 + rotated[digit]
+            n180 = n180 * 10 + rotatable_digits[digit]
         return n != n180
