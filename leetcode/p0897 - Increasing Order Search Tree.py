7efa624dbbe96a2ce0a76ed4cf03bf21db5eee2a
Alexander Liseyko <liseyko@gmail.com>
2019-08-29 23:16:26 -0700

    modified:   p0897 - Increasing Order Search Tree.py

--- leetcode/p0897 - Increasing Order Search Tree.py
+++ leetcode/p0897 - Increasing Order Search Tree.py
@@ -1,17 +1,14 @@
 class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        new_root = self.tail = TreeNode('dummy')
-
         def dfs(root):
             if not root:
                 return None
-
             dfs(root.left)
+            root.left = None
             self.tail.right = root
             self.tail = self.tail.right
-            self.tail.left = None
-            rc, self.tail.right = self.tail.right, None
-            dfs(rc)
+            dfs(root.right)
 
+        new_root = self.tail = TreeNode('dummy')
         dfs(root)
         return new_root.right
