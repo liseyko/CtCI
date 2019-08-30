960f081de52dfd55de96d2e2cc4dd8d798545585
Alexander Liseyko <liseyko@gmail.com>
2019-08-30 01:07:51 -0700

    modified:   p0836 - Rectangle Overlap.py

--- leetcode/p0836 - Rectangle Overlap.py
+++ leetcode/p0836 - Rectangle Overlap.py
@@ -1,17 +1,9 @@
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
-        r1x = sorted(rec1[0::2])
-        r1y = sorted(rec1[1::2])
-        r2x = sorted(rec2[0::2])
-        r2y = sorted(rec2[1::2])
-        return any(r1x[0] < x < r1x[1] for x in r2x) and\
-            any(r1y[0] < y < r1y[1] for y in r2y) or\
-            any(r2x[0] < x < r2x[1] for x in r1x) and\
-            any(r2y[0] < y < r2y[1] for y in r1y) or\
-            r2x[0] <= r1x[0] and r2x[1] >= r1x[1] and\
-            r1y[0] <= r2y[0] and r1y[1] >= r2y[1] or\
-            r2y[0] <= r1y[0] and r2y[1] >= r1y[1] and\
-            r1x[0] <= r2x[0] and r1x[1] >= r2x[1]
+        return (any(rec1[0] < x < rec1[2] for x in rec2[0::2]) or
+                any(rec2[0] < x < rec2[2] for x in rec1[0::2])) and\
+               (any(rec1[1] < y < rec1[3] for y in rec2[1::2]) or
+                any(rec2[1] < y < rec2[3] for y in rec1[1::2]))
 
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
         return not(rec1[0] >= rec2[2] or
