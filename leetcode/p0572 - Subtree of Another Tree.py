78666920c5d2af6e969e4de2b8b38cba3214ed03
Alexander Liseyko <liseyko@gmail.com>
2019-08-30 23:29:15 -0700

    new file:   p0572 - Subtree of Another Tree.py

---
+++ leetcode/p0572 - Subtree of Another Tree.py
@@ -0,0 +1,33 @@
+class Solution:
+    """ second: tree2list, then find sublist by bruteforce anyway"""
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        ss, ts = [], []
+
+        def dfs(n, st):
+            if not n:
+                st.append(None)
+                return
+            st.append(n.val)
+            dfs(n.left, st)
+            dfs(n.right, st)
+
+        dfs(s, ss)
+        dfs(t, ts)
+        for i in range(len(ss) - len(ts)+1):
+            if ts == ss[i:i+len(ts)]:
+                return True
+        return False
+
+class Solution:
+    """ first bruteforce solution """
+    def _isSameTree(self, s: TreeNode, t: TreeNode) -> bool:
+        if getattr(s, 'val', None) != getattr(t, 'val', None):
+            return False
+        return s == t or\
+            self._isSameTree(s.left, t.left) and\
+            self._isSameTree(s.right, t.right)
+
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        return self._isSameTree(s, t) or s and\
+            (self.isSubtree(s.left, t) or
+             self.isSubtree(s.right, t))
