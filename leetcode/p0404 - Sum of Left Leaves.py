3627f95a42fe9f8b57263bf40b5d7925f06b9cde
Alexander Liseyko <liseyko@gmail.com>
2019-08-30 22:13:20 -0700

    modified:   p0404 - Sum of Left Leaves.py

--- leetcode/p0404 - Sum of Left Leaves.py
+++ leetcode/p0404 - Sum of Left Leaves.py
@@ -5,9 +5,18 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    def sumOfLeftLeaves(self, root: TreeNode) -> int:
 
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode, isLeft=False) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right and isLeft:
+            return root.val
+        return self.sumOfLeftLeaves(root.left, True) +\
+            self.sumOfLeftLeaves(root.right)
+
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        """it was just a first thought:"""
         def _isLeaf(node):
             return node and not node.left and not node.right
 
