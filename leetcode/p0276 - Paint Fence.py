0e1995f3ace9a9b688bb858fa2e1a7a4a7d59574
Alexander Liseyko <liseyko@gmail.com>
2019-08-31 23:55:36 -0700

    modified:   p0276 - Paint Fence.py

--- leetcode/p0276 - Paint Fence.py
+++ leetcode/p0276 - Paint Fence.py
@@ -1,9 +1,7 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return k
+        if n < 2:
+            return n*k
         same, diff = k, k*(k-1)
         for _ in range(n-2):
             same, diff = diff, (same+diff)*(k-1)
