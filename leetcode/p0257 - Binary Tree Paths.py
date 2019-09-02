fc5625b281d7b69c818e5efb420a76d3634f555e
Alexander Liseyko <liseyko@gmail.com>
2019-09-01 20:15:07 -0700

    modified:   p0257 - Binary Tree Paths.py

--- leetcode/p0257 - Binary Tree Paths.py
+++ leetcode/p0257 - Binary Tree Paths.py
@@ -14,3 +14,24 @@ class Solution:
         self.res = []
         dfs()
         return self.res
+
+
+class Solution:
+    """same, but faster"""
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+
+        def dfs(r=root, path=[]):
+            if not r:
+                return
+            path.append(str(r.val))
+            if not r.left and not r.right:
+                self.res.append(''.join(path))
+            path.append('->')
+            dfs(r.left, path)
+            dfs(r.right, path)
+            del path[-2:]
+
+        self.res = []
+        dfs()
+
+        return self.res
