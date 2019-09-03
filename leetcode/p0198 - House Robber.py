16cb46fe05ad8ea8c2ca5f4814cbaacf4e2f8e59
Alexander Liseyko <liseyko@gmail.com>
2019-09-03 03:04:30 -0700

    modified:   p0198 - House Robber.py

--- leetcode/p0198 - House Robber.py
+++ leetcode/p0198 - House Robber.py
@@ -1,10 +1,12 @@
 class Solution:
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        prevMax = currMax = 0
+    def rob(self, nums: List[int]) -> int:
+        nums += [0]*2
+        for i in range(2, len(nums)):
+            nums[i] += max(nums[i-2], nums[i-3])
+        return max(nums[-2:])
+
+    def rob(self, nums: List[int]) -> int:
+        prv = cur = 0
         for n in nums:
-            prevMax, currMax = currMax, max(prevMax + n, currMax)
-        return currMax
+            prv, cur = cur, max(cur, prv+n)
+        return cur
