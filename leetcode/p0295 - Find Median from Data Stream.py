17b38382a59ef264ebdcf06e4562666baf70e7d1
Alexander Liseyko <liseyko@gmail.com>
2019-09-03 13:53:20 -0700

    modified:   p0295 - Find Median from Data Stream.py

--- leetcode/p0295 - Find Median from Data Stream.py
+++ leetcode/p0295 - Find Median from Data Stream.py
@@ -1,20 +1,27 @@
 class MedianFinder:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.nums = []
 
     def addNum(self, num: int) -> None:
         bisect.insort(self.nums, num)
 
     def findMedian(self) -> float:
-        return (self.nums[(len(self.nums)-1)//2]+\
-                self.nums[len(self.nums)//2]) / 2
+        return (self.nums[(len(self.nums)-1) // 2] +
+                self.nums[len(self.nums) // 2]) / 2
 
 
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+class MedianFinder:
+
+    def __init__(self):
+        self.minh, self.maxh = [], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.minh, num)
+        heapq.heappush(self.maxh, -heapq.heappop(self.minh))
+        if len(self.minh) < len(self.maxh):
+            heapq.heappush(self.minh, -heapq.heappop(self.maxh))
+
+    def findMedian(self) -> float:
+        return self.minh[0] if len(self.minh) > len(self.maxh) else\
+               (self.minh[0] - self.maxh[0]) / 2
