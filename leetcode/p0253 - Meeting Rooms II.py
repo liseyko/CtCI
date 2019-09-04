baf5b13ef05fffed9ce5dc4e2fd0e2a58fa7e35c
Alexander Liseyko <liseyko@gmail.com>
2019-09-04 16:13:46 -0700

    modified:   p0253 - Meeting Rooms II.py

--- leetcode/p0253 - Meeting Rooms II.py
+++ leetcode/p0253 - Meeting Rooms II.py
@@ -2,6 +2,19 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         h = []
         res = cur = 0
+        for tf in sorted(intervals):
+            while h and h[0] <= tf[0]:
+                heapq.heappop(h)
+                cur -= 1
+            heapq.heappush(h, (tf[1]))
+            cur += 1
+            res = max(res, cur)
+        return res
+
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        '''1st try'''
+        h = []
+        res = cur = 0
         for tf in intervals:
             heapq.heappush(h, (tf[0], 1))
             heapq.heappush(h, (tf[1], -1))
