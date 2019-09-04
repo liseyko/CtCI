c4697f00dca2aa89c25411dab5b41c7a9085b230
Alexander Liseyko <liseyko@gmail.com>
2019-09-04 15:18:30 -0700

    modified:   p0986 - Interval List Intersections.py

--- leetcode/p0986 - Interval List Intersections.py
+++ leetcode/p0986 - Interval List Intersections.py
@@ -16,6 +16,18 @@ class Solution:
 
         return res
 
+    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        i = j = 0
+        res = []
+        while i < len(A) and j < len(B):
+            intersection = [max(A[i][0], B[j][0]), min(A[i][1], B[j][1])]
+            if intersection[0] <= intersection[1]:
+                res.append(intersection)
+            if A[i][1] < B[j][1]:
+                i += 1
+            else:
+                j += 1
+        return res
 
 class Solution:
     def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
