a8404163cdc792374c72564c3a2ef7ed23680a6b
Alexander Liseyko <liseyko@gmail.com>
2019-09-06 14:42:20 -0700

    modified:   p0154 - Find Minimum in Rotated Sorted Array II.py

--- leetcode/p0154 - Find Minimum in Rotated Sorted Array II.py
+++ leetcode/p0154 - Find Minimum in Rotated Sorted Array II.py
@@ -10,3 +10,16 @@ class Solution:
             else:
                 low, hgh = low+1, hgh-1
         return nums[low]
+
+    def findMin(self, nums: List[int]) -> int:
+        i, j = 0, len(nums)-1
+        while i < j and nums[i] >= nums[j]:
+            if nums[i] == nums[j]:
+                j -= 1
+                continue
+            m = i + (j-i)//2
+            if nums[i] > nums[m]:
+                j = m
+            else:
+                i = m+1
+        return nums[i]
