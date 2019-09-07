970d0efe41fca055e5647a8cda9eaa643fab4666
Alexander Liseyko <liseyko@gmail.com>
2019-09-06 19:11:51 -0700

    modified:   p0125 - Valid Palindrome.py

--- leetcode/p0125 - Valid Palindrome.py
+++ leetcode/p0125 - Valid Palindrome.py
@@ -1,8 +1,6 @@
 class Solution:
-    def isPalindrome(self, s):
-        if len(s) < 2:
-            return True
-        i, j = 0, len(s) - 1
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s)-1
         while i < j:
             if not s[i].isalnum():
                 i += 1
@@ -11,5 +9,5 @@ class Solution:
             else:
                 if s[i].lower() != s[j].lower():
                     return False
-                i, j = i + 1, j - 1
+                i, j = i+1, j-1
         return True
