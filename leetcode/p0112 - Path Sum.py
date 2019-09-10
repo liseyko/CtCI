d67f7c6393189e1022786bc1fe2f4fb5c64db733
Alexander Liseyko <liseyko@gmail.com>
2019-09-10 12:34:52 -0700

    modified:   p0112 - Path Sum.py

--- leetcode/p0112 - Path Sum.py
+++ leetcode/p0112 - Path Sum.py
@@ -1,17 +1,9 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
 class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         if not root:
             return False
+        sum -= root.val
         if root.left or root.right:
-            return self.hasPathSum(root.left, sum - root.val) \
-                or self.hasPathSum(root.right, sum - root.val)
-        else:
-            return sum == root.val
+            return self.hasPathSum(root.left, sum) or\
+                self.hasPathSum(root.right, sum)
+        return sum == 0
