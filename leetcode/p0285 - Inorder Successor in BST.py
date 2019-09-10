0cdc7e04daa8ebf7c3f4a9aff7eba899dbda9579
Alexander Liseyko <liseyko@gmail.com>
2019-09-10 14:53:40 -0700

    modified:   p0285 - Inorder Successor in BST.py

--- leetcode/p0285 - Inorder Successor in BST.py
+++ leetcode/p0285 - Inorder Successor in BST.py
@@ -5,27 +5,7 @@ class Solution:
             while root:
                 stack.append(root)
                 root = root.left
-            curNode = stack[-1]
-            if curNode.val > p.val:
-                return curNode
-            root = stack.pop().right
-
-    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
-
-        def dfs_find(root=root, n=p):
-            if not root:
-                return
-            dfs_find(root.left)
-            if self.res:
-                return
-            if root == n:
-                self.found = True
-            elif self.found:
-                self.res = root
-                return
-            dfs_find(root.right)
-
-        self.found = False
-        self.res = None
-        dfs_find()
-        return self.res
+            root = stack.pop()
+            if root.val > p.val:
+                return root
+            root = root.right
