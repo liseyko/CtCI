172fb27c13b846dcdaedcf529d29580be101d65e
Alexander Liseyko <liseyko@gmail.com>
2019-09-10 12:48:52 -0700

    modified:   p0113 - Path Sum II.py

--- leetcode/p0113 - Path Sum II.py
+++ leetcode/p0113 - Path Sum II.py
@@ -1,37 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    def __init__(self):
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+
+        def dfs(root: TreeNode, sum: int, path=[]) -> None:
+            if not root:
+                return
+            sum, path = sum-root.val, path+[root.val]
+            if not root.left and not root.right and sum == 0:
+                self.res.append(path)
+            else:
+                dfs(root.left, sum, path)
+                dfs(root.right, sum, path)
+
         self.res = []
-
-    def _pathSum(self, root, sum):
-        sum -= root.val
-        if sum == 0 and not root.left and not root.right:
-            r = [root.val]
-            while root.parent:
-                r.append(root.parent.val)
-                root = root.parent
-
-            self.res.append(r[::-1])
-            return
-
-        for c in root.left, root.right:
-            if c:
-                c.parent = root
-                self._pathSum(c, sum)
-
-    def pathSum(self, root, sum):
-        """
-        :type root: TreeNode
-        :type sum: int
-        :rtype: List[List[int]]
-        """
-        if not root: return []
-        root.parent = None
-        self._pathSum(root, sum)
+        dfs(root, sum)
         return self.res
