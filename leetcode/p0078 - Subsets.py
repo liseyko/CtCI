916bd1cf8616646d5c2ba6956664be90e01dc7bc
Alexander Liseyko <liseyko@gmail.com>
2019-09-11 11:05:50 -0700

    modified:   p0078 - Subsets.py

--- leetcode/p0078 - Subsets.py
+++ leetcode/p0078 - Subsets.py
@@ -1,4 +1,11 @@
 class Solution:
+    def subsets(self, nums):
+        res = [[]]
+        for num in nums:
+            res += [item+[num] for item in res]
+        return res
+
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
         def bt(i=0, buf=[]):
@@ -11,3 +18,24 @@ class Solution:
         self.res = []
         bt()
         return self.res
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        def bt(i=0, buf=[]):
+            self.res.append(buf[:])
+            for j in range(i, len(nums)):
+                bt(j+1, buf+[nums[j]])
+
+        self.res = []
+        bt()
+        return self.res
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        for mask in range(2**len(nums)):
+            curSet = []
+            for bit in range(len(nums)):
+                if mask & 1 << bit:
+                    curSet.append(nums[bit])
+            res.append(curSet)
+        return res
