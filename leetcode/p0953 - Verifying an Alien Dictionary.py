82b1893f0a7e362c02ff8b34d994f7faba63c364
Alexander Liseyko <liseyko@gmail.com>
2019-09-12 00:07:23 -0700

    new file:   p0953 - Verifying an Alien Dictionary.py

---
+++ leetcode/p0953 - Verifying an Alien Dictionary.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order = {c: i for i, c in enumerate(order)}
+        for i in range(1, len(words)):
+            w1, w2 = words[i-1], words[i]
+            for i in range(max(len(w1), len(w2))):
+                if i == len(w1):
+                    return True
+                if i == len(w2) or order[w1[i]] > order[w2[i]]:
+                    return False
+                elif order[w1[i]] < order[w2[i]]:
+                    break
+        return True
