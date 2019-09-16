9c190d3fc3f500cdd03759e482d2073e241a1d1a
Alexander Liseyko <liseyko@gmail.com>
2019-09-16 08:08:44 -0700

    modified:   p0821 - Shortest Distance to a Character.py

--- leetcode/p0821 - Shortest Distance to a Character.py
+++ leetcode/p0821 - Shortest Distance to a Character.py
@@ -1,5 +1,23 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
+        if s:
+            res = [0] if s[0] == c else [float('inf')]
+        else:
+            res = []
+
+        for i in range(1, len(s)):
+            if s[i] == c:
+                res.append(0)
+                for j in range(i-1, -1, -1):
+                    if res[j] <= res[j+1]:
+                        break
+                    res[j] = res[j+1]+1
+            else:
+                res.append(res[i-1]+1)
+
+        return res
+
+    def shortestToChar(self, s: str, c: str) -> List[int]:
         res = []
         substr = []
         cnt = 0
@@ -7,11 +25,11 @@ class Solution:
             cnt += 1
             if curChar == c:
                 if res:
-                    res += [i for i in range(1,cnt//2+cnt%2)]
-                    res += [i for i in range(cnt//2,-1,-1)]
+                    res += [i for i in range(1, cnt//2 + cnt % 2)]
+                    res += [i for i in range(cnt//2, -1, -1)]
                 else:
-                    res = [i for i in range(cnt-1,-1,-1)]
+                    res = [i for i in range(cnt-1, -1, -1)]
                 cnt = 0
-        res += [i for i in range(1,cnt+1)]
+        res += [i for i in range(1, cnt+1)]
 
-        return res # if c in s else [float('inf')]*len(s)
+        return res  # if c in s else [float('inf')]*len(s)
