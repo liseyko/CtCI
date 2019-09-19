66fa5068430b26e143bb286be879b39660592733
Alexander Liseyko <liseyko@gmail.com>
2019-09-19 14:03:06 -0700

    modified:   p0743 - Network Delay Time.py

--- leetcode/p0743 - Network Delay Time.py
+++ leetcode/p0743 - Network Delay Time.py
@@ -1,21 +1,39 @@
 class Solution:
-    ''' Dijkstra '''
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
         g = [[] for _ in range(N+1)]
         for u, v, w in times:
             g[u].append((w, v))
-        dist = {}
+        visited = wgts = {}
         routes = [(0, K)]
         while routes:
-            src_dist, src = heapq.heappop(routes)
-            if src in dist:
+            src_w, src = heapq.heappop(routes)
+            if src in visited:
                 continue
-            dist[src] = src_dist
-            for dst_dist, dst in g[src]:
-                if dst not in dist:
-                    heapq.heappush(routes, (src_dist+dst_dist, dst))
+            wgts[src] = src_w
+            for dst_w, dst in g[src]:
+                if dst not in visited:
+                    heapq.heappush(routes, (src_w+dst_w, dst))
 
-        return max(dist.values()) if len(dist)==N else -1
+        return max(wgts.values()) if len(visited)==N else -1
+class Solution:
+    ''' BFS '''
+    def networkDelayTime(self, times, N, K):
+        wgts = [-1]+[float("inf")] * N
+        adj = [[] for _ in range(N+1)]
+        q = collections.deque([(K, 0)])
+
+        for u, v, w in times:
+            adj[u].append((v, w))
+            
+        while q:
+            u, uw = q.popleft()
+            if uw < wgts[u]:
+                wgts[u] = uw
+                for v, vw in adj[u]:
+                    q.append((v, uw + vw))
+        mx = max(wgts)
+        return mx if mx < float("inf") else -1
+
 
 class Solution:
     ''' DFS '''
@@ -24,14 +42,13 @@ class Solution:
         for u, v, w in times:
             g[u].append((w, v))
         dist = {}
-        
+
         def sendPacket(src=K, ctime=0):
             dist[src] = ctime
             for t, dst in sorted(g[src]):
                 if dst in dist and dist[dst] <= ctime+t:
                     continue
                 sendPacket(dst, ctime+t)
-                
+
         sendPacket()
-        #print(vis) 
         return max(dist.values()) if len(dist)==N else -1
