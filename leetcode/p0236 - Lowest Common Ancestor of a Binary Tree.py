9a34ecfae8f789221f44ab7af4fcf86908a63f86
Alexander Liseyko <liseyko@gmail.com>
2019-09-20 10:07:52 -0700

    modified:   p0236 - Lowest Common Ancestor of a Binary Tree.py

--- leetcode/p0236 - Lowest Common Ancestor of a Binary Tree.py
+++ leetcode/p0236 - Lowest Common Ancestor of a Binary Tree.py
@@ -1,4 +1,26 @@
 class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        lca_idx, found1 = 0, False
+        treeNodeChldNameList = ['right', 'left']
+        stack = [(root, treeNodeChldNameList[:])] if root else None
+        while stack:
+            node, unprocChldrn = stack[-1]
+            if len(unprocChldrn) == 2 and (node == p or node == q):
+                if found1:
+                    return stack[lca_idx][0]
+                found1 = True
+                lca_idx = len(stack)-1
+
+            if unprocChldrn:
+                nxtNode = getattr(node, unprocChldrn.pop())
+                if nxtNode:
+                    stack.append((nxtNode, treeNodeChldNameList[:]))
+            else:
+                del stack[-1]
+                lca_idx = min(lca_idx, len(stack)-1)
+
+
+class Solution:
     def lowestCommonAncestor(self, root, p, q):
         lca = None
         stk = [(root, [root.right, root.left])]
@@ -100,26 +122,3 @@ class Solution:
                 stack.pop()
                 if found and result == node:
                     result = stack[-1][0] if stack else None
-
-
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        chldNames = ['right', 'left']
-        nodes = [(root, chldNames[:])] if root else None
-        lca_index, found = None, False
-        while nodes:
-            node, children = nodes[-1]
-            if len(children) == 2 and node in (p, q):
-                if found:
-                    return nodes[lca_index][0]
-                found = True
-                lca_index = len(nodes)-1
-            if not children:
-                nodes.pop()
-                if found and lca_index == len(nodes):
-                    lca_index -= 1
-            else:
-                c = getattr(node, children.pop())
-                if c:
-                    nodes.append((c, chldNames[:]))
-        return None
