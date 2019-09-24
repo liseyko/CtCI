bd9e49fb486aaebaf545320a672960295e622a2c
Alexander Liseyko <liseyko@gmail.com>
2019-09-24 13:20:17 -0700

    modified:   p0528 - Random Pick with Weight.py

--- leetcode/p0528 - Random Pick with Weight.py
+++ leetcode/p0528 - Random Pick with Weight.py
@@ -11,3 +11,14 @@ class Solution:
         w = random.randint(0, self.maxWeight-1)
         idx = bisect.bisect(self.idxs, w)
         return idx
+
+    def pickIndex(self) -> int:
+        w = random.randint(0, self.maxWeight-1)
+        li, ri = 0, len(self.idxs)-1
+        while li < ri:
+            mi = li + (ri-li) // 2
+            if w <= self.idxs[mi]-1:
+                ri = mi
+            else:
+                li = mi+1
+        return li
